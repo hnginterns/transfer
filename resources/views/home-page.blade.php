ce651c1887673e8874577e6e6f804a5831be6ad8
Idorenyin Obong <idee4ril@gmail.com>
2017-09-19 20:16:35 +0100

    added home page

---
+++ resources/views/home-page.blade.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homepage</title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/../../public/css/style.css">
+</head>
+<body>
+    <!-- HEADER BEGINS -->
+    <div id="header">
+        <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><img id="logo" src="/../../public/img/logo.png" alt="Company logo" style="display: inline;"> Transfer Rules</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+              <li><a href="#">FAQs</a></li>
+              <li><a href="#">Features</a></li>
+              <li><a href="#">Demo</a></li>
+              <li id="sign-in"><a href="#">SIGN IN</a></li>              
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
+
+      <div id="header-content">
+          <img src="img/phone.png" alt="Transfer rules mobile app screenshot" id="mobile-app">
+          <div id="content-text" class="clearfix">
+                <span id="heading-hd">
+                    Create an account for your company with PaysFund
+                </span>
+                <span id="text-hd">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
+                </span>
+                <button id="button-hd">
+                    DOWNLOAD APP
+                </button>
+          </div>
+      </div>
+    </div>
+    <!--HEADER ENDS -->
+
+    <!-- SECTION ONE - (AMAZING FEATURES) --> 
+    <div id="section1">
+        <p class="section-header">
+            Amazing Features
+        </p>    
+        <p class="section-sub">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim             
+        </p>
+        <div id="features">
+            <img src="img/screenshots.png" alt="site screenshot" id="screenshots">
+            <div id="features-list">
+                <div class="feature">
+                    <span class="feature-img"></span>
+                    <span id="feature-heading">Get Payment</span>
+                    <span class="feature-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                </div>
+                <div class="feature">
+                    <span class="feature-img"></span>
+                    <span id="feature-heading">Manage Funds</span>
+                    <span class="feature-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                </div>
+                <div class="feature">
+                    <span class="feature-img"></span>
+                    <span id="feature-heading">Access Wallet</span>
+                    <span class="feature-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                </div>
+                <div class="feature">
+                    <span class="feature-img"></span>
+                    <span id="feature-heading">Multiple User Access</span>
+                    <span class="feature-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- SECTION ONE ENDS -->
+
+
+     <!-- SECTION TWO - (TESTIMONIES) --> 
+    <div id="section2">
+        <p class="section-header">
+            Testimonies
+        </p>    
+        <p class="section-sub">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+        </p>
+        
+        <div id="t-container">
+            <div class="row">
+                <div class="media col-md-6" id="media1">
+                    <a class="pull-left" href="#">
+                        <span class="media-object testimony-img"></span>                
+                    </a>
+                    <div class="media-body"> 
+                        <h4 class="media-heading name">Name</h4>
+                        <p class="testimony">Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="media col-md-12 pull-right" id="media2">
+                    <a class="pull-right" href="#">
+                        <span class="media-object testimony-img"></span>                
+                    </a>
+                    <div class="media-body"> 
+                        <h4 class="media-heading name">Name</h4>
+                        <p class="testimony">Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i</p>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </div>
+     <!-- SECTION TWO ENDS -->    
+     
+    <!-- DIVIDER --> 
+        <div id="divider">
+            <div id="divider-content">
+                <div id="divider-text">
+                    <p id="divider-title">Imagine your companies future</p>
+                    <p id="">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing
+                    </p>
+                </div>
+                <button id="divider-cta">GET STARTED</button>
+            </div>
+        </div>
+    <!-- DIVIDER ENDS-->
+
+    <!-- FOOTER -->
+
+    <div id="footer">
+        <div id="footer-heading">
+            <li><a href="#">Lorem ipsum dolor</a></li>        
+            <li><a href="#">Lorem ipsum dolor</a></li>
+            <li><a href="#">Lorem ipsum dolor</a></li>
+            <li><a href="#">Lorem ipsum dolor</a></li> 
+        </div>
+        <div id="footer-links">
+            <li><a href="#">Lorem ipsum dolor</a></li>        
+            <li><a href="#">Lorem ipsum dolor</a></li>
+            <li><a href="#">Lorem ipsum dolor</a></li>
+            <li><a href="#">Lorem ipsum dolor</a></li> 
+            <li><a href="#">Lorem ipsum dolor</a></li>        
+            <li><a href="#">Lorem ipsum dolor</a></li>
+            <li><a href="#">Lorem ipsum dolor</a></li>
+            <li><a href="#">Lorem ipsum dolor</a></li> 
+            <li><a href="#">Lorem ipsum dolor</a></li>        
+            <li><a href="#">Lorem ipsum dolor</a></li>
+            <li><a href="#">Lorem ipsum dolor</a></li>
+            <li><a href="#">Lorem ipsum dolor</a></li>    
+        </div>    
+        <p id="line"> </p>
+        <div id="lower-footer">
+            <p>&#169; 2017 Transferrules.com. All rights reserved</p>
+        </div>
+    </div>
+
+    <!-- FOOTER BEGINS -->
+    <script
+        src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+        crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('home-page');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
