4efebbe5c6f46a51fd7adc7c8b94481260d5f74b
Okwu Joscelyn Kinikachi <ojk564@gmail.com>
2017-09-19 17:11:38 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/balance', 'GetWalletController@getAccessToken')->name('balance');
+Route::get('/balance', 'GetWalletController@getWalletBalance')->name('balance');
