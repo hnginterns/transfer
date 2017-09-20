be152ef6856622b1fd82801d40256656889c21a2
icnwakanma <31223868+icnwakanma@users.noreply.github.com>
2017-09-20 14:47:53 +0100

    Update BanksController.php

--- app/Http/Controllers/BanksController.php
+++ app/Http/Controllers/BanksController.php
@@ -8,8 +8,7 @@ class BanksController extends Controller
     
     public function banks()
     {
-        \Unirest\Request::verifyPeer(false); 
-
+        
         $headers = array('content-type' => 'application/json');
 
         $response = \Unirest\Request::post('https://moneywave.herokuapp.com/banks', $headers);
