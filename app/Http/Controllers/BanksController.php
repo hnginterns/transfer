f47cc952b563af10ddc2e25485630c0d94b7e961
= <=>
2017-09-20 16:34:04 +0100

    m

--- app/Http/Controllers/BanksController.php
+++ app/Http/Controllers/BanksController.php
@@ -10,8 +10,7 @@ class BanksController extends Controller
     public function banks()
     {
         
-        \Unirest\Request::verifyPeer(false); 
-
+       
         $headers = array('content-type' => 'application/json');
 
         $response = \Unirest\Request::post('https://moneywave.herokuapp.com/banks', $headers);
