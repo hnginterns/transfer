ee6339f5ef629c6ee7c4ec8ae8c59cec38f7063a
Franklin Nwanze <nwanzechuka1993@gmail.com>
2017-09-20 16:52:49 +0100

    added rule model and seeders and made a change in restriction migration

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,11 +27,9 @@ $factory->define(App\User::class, function (Faker $faker) {
 });
 
 $factory->define(App\Wallet::class, function (Faker $faker) {
-        // App\User::all()->random()->id;
-
     return [
         'user_id' => App\User::all()->random()->id,
-        'balance' => $faker->numberBetween(10000, 1900000),
+        'balance' => $faker->numberBetween(1000, 19000000),
         'wallet_code' => str_random(20),
         'created_by' => rand(1, 80),
         'updated_by' => rand(1, 80),
@@ -43,8 +41,17 @@ $factory->define(App\Restriction::class, function (Faker $faker) {
     return [
         'user_id' => App\User::all()->random()->id,
         'wallet_id' => App\Wallet::all()->random()->id,
-        'max_amount' => rand(200000, 2000000),
+        'rule_id' => App\Rule::all()->random()->id,
+    ];
+});
+
+$factory->define(App\Rule::class, function (Faker $faker) {
+    
+    return [
         'min_amount' => rand(0, 1000),
+        'max_amount' => rand(200000, 2000000),
+        'max_transactions_per_day' => rand(0, 50),
+        'max_amount_transfer_per_day' => $faker->numberBetween(100000, 1900000),
         'created_by' => $faker->lastName,
         'updated_by' => $faker->firstName,
     ];
@@ -57,7 +64,7 @@ $factory->define(App\Transaction::class, function (Faker $faker) {
         'amount_transfered' => rand(5000, 2000000),
         'payer_name' => $faker->firstName,
         'payee_name' => $faker->firstName,
-        'payee_account_number' => $faker->numberBetween(1000, 1900000),
+        'payee_account_number' => $faker->numberBetween(1000000000, 9999999999),
         'payee_bank' => $faker->firstName.'  '.$faker->lastName,
         'transaction_reference'=> str_random(24),
         'payee_wallet_code' => App\Wallet::all()->random()->wallet_code,
