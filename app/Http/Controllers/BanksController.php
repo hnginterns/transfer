11ed550d8d76ca7693e79f63ff351bdcfbf1d678
= <=>
2017-09-21 16:19:53 +0100

    a

--- app/Http/Controllers/BanksController.php
+++ app/Http/Controllers/BanksController.php
@@ -11,7 +11,6 @@ class BanksController extends Controller
     {
       
 
-        \Unirest\Request::verifyPeer(false);
         
 
         $headers = array('content-type' => 'application/json', 'Authorization' => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQwLCJuYW1lIjoic2F2YW5hIHNtYXJ0c2F2ZSIsImFjY291bnROdW1iZXIiOiIiLCJiYW5rQ29kZSI6Ijk5OSIsImlzQWN0aXZlIjp0cnVlLCJjcmVhdGVkQXQiOiIyMDE2LTEyLTA4VDEwOjM4OjE5LjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDE3LTA2LTE0VDEzOjAxOjQ5LjAwMFoiLCJkZWxldGVkQXQiOm51bGwsImlhdCI6MTQ5ODMzNTE2NSwiZXhwIjoxNDk4MzQyMzY1fQ.WojvkYOC2j6XTUfg_E4WQkxQChPUyCgYUCIKaW83YXA');
