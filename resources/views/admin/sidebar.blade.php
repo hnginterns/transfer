e136434a5aa69f29462edf66954f2d21f6ce85d0
stephenjude <stephenjudesuccess@gmail.com>
2017-09-21 09:43:54 +0100

    Install bootstrap admin template

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,31 @@
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="{{ asset("/bower_components/AdminLTE/dist/img/user2-160x160.jpg") }}" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Transfer Rule Admin</p>
+          <!-- Status -->
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+
+
+      <!-- Sidebar Menu -->
+      <ul class="sidebar-menu">
+        <!-- Optionally, you can add icons to the links -->
+        <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Wallets</span></a></li>
+        <li><a href="#"><i class="fa fa-link"></i> <span></span>Accounts</a></li>
+        <li><a href="#"><i class="fa fa-link"></i> <span></span>Transactions</a></li>
+
+      </ul>
+      <!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+  </aside>
