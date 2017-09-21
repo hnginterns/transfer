9974abe8d39b98a7e254a89384f503982a5c929c
= <=>
2017-09-21 16:21:32 +0100

    test

--- app/Http/Controllers/BanksController.php
+++ app/Http/Controllers/BanksController.php
@@ -13,11 +13,12 @@ class BanksController extends Controller
 
         
 
-        $headers = array('content-type' => 'application/json', 'Authorization' => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQwLCJuYW1lIjoic2F2YW5hIHNtYXJ0c2F2ZSIsImFjY291bnROdW1iZXIiOiIiLCJiYW5rQ29kZSI6Ijk5OSIsImlzQWN0aXZlIjp0cnVlLCJjcmVhdGVkQXQiOiIyMDE2LTEyLTA4VDEwOjM4OjE5LjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDE3LTA2LTE0VDEzOjAxOjQ5LjAwMFoiLCJkZWxldGVkQXQiOm51bGwsImlhdCI6MTQ5ODMzNTE2NSwiZXhwIjoxNDk4MzQyMzY1fQ.WojvkYOC2j6XTUfg_E4WQkxQChPUyCgYUCIKaW83YXA');
+        $headers = array('content-type' => 'application/json');
         $response = \Unirest\Request::post('https://moneywave.herokuapp.com/banks', $headers);
         $data = json_decode($response->raw_body, TRUE);
         $banks = $data['data'];
-        //die();
+        var_dump($response);
+        die();
         
         return view('banks', compact('banks'));
     }
