889672c894db9453afcea054d52c290bfc1098b0
= <=>
2017-09-21 16:42:28 +0100

    logic to fetch list of banks

--- app/Http/Controllers/BanksController.php
+++ app/Http/Controllers/BanksController.php
@@ -9,20 +9,12 @@ class BanksController extends Controller
     
     public function banks()
     {
-      
+    
 
-        function _isCurl(){
-    return function_exists('curl_version');
-}
-
-        var_dump('Current PHP version: ' . phpversion());
-        die();
         $headers = array('content-type' => 'application/json');
         $response = \Unirest\Request::post('https://moneywave.herokuapp.com/banks', $headers);
         $data = json_decode($response->raw_body, TRUE);
         $banks = $data['data'];
-        var_dump($response);
-        die();
         
         return view('banks', compact('banks'));
     }
