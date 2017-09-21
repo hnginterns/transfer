16d431a4487a1bb95a5e5addcbba48cc655cde08
= <=>
2017-09-21 16:28:51 +0100

    test

--- app/Http/Controllers/BanksController.php
+++ app/Http/Controllers/BanksController.php
@@ -11,8 +11,12 @@ class BanksController extends Controller
     {
       
 
-        
+        function _isCurl(){
+    return function_exists('curl_version');
+}
 
+        var_dump(_isCurl());
+        die();
         $headers = array('content-type' => 'application/json');
         $response = \Unirest\Request::post('https://moneywave.herokuapp.com/banks', $headers);
         $data = json_decode($response->raw_body, TRUE);
