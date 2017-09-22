51a06e239c23037f677eb6c7e676d96d904c4a0e
Egberinde Olatunbosun <bosunski@gmail.com>
2017-09-22 16:25:53 +0100

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,7 +11,7 @@ class AdminController extends Controller
     }
 
 	public function index() {
-		return view('admin.home');
+		return view('admin.dashboard');
 	}
 
     public function setRule() {
