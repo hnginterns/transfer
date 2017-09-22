ee6866775ce7cac575b07896563583cc29b9aef4
Okwu Joscelyn Kinikachi <ojk564@gmail.com>
2017-09-22 23:37:49 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -26,50 +26,42 @@ Route::get('/confirmation', 'ValidateAccountController@confirm');
 
 Route::get('/404', 'pagesController@pagenotfound');
 
-
-// admin routes
-Route::get('/manager', 'AdminController@index');
-Route::get('/view-accounts', 'pagesController@viewAccounts');
-Route::get('/addaccount', 'AdminController@addaccount');
-Route::get('/usermanagement', 'AdminController@usermanagement');
-Route::get('/web-analytics', 'pagesController@webAnalytics');
-Route::get('/banks', 'BanksController@banks');
-Route::get('/success', 'pagesController@success');
-Route::get('/failed', 'pagesController@failed');
-Route::get('/transfer', 'pagesController@transfer');
-Route::get('/balance', 'pagesController@balance');
-
-
-//User routes
-Route::get('/userdashboard', 'pagesController@userdashboard');
-Route::get('/transfer-to-bank', 'pagesController@bank_transfer');
-Route::get('/transfer-to-wallet', 'pagesController@wallet_transfer');
-Route::get('/wallet-view', 'pagesController@viewWallet')->name('wallet');
-Route::get('/banks', 'BanksController@banks');
-Route::get('/success', 'pagesController@success');
-Route::get('/failed', 'pagesController@failed');
-Route::get('/transfer', 'pagesController@transfer');
-Route::get('/balance', 'pagesController@balance');
-
-
-
 // authentications
 Route::group(['middleware' => 'auth'], function() {
-
+	//User routes
+	Route::get('/userdashboard', 'pagesController@userdashboard');
+	Route::get('/transfer-to-bank', 'pagesController@bank_transfer');
+	Route::get('/transfer-to-wallet', 'pagesController@wallet_transfer');
+	Route::get('/wallet-view', 'pagesController@viewWallet')->name('wallet');
+	Route::get('/banks', 'BanksController@banks');
+	Route::get('/success', 'pagesController@success');
+	Route::get('/failed', 'pagesController@failed');
+	Route::get('/transfer', 'pagesController@transfer');
+	Route::get('/balance', 'pagesController@balance');
 });
 
 // auth admin
 Route::group(['middleware' => ['auth', 'admin']], function() {
-		// get manager
+	// get manager
+	// Set rules that users will transfer with
+	Route::get('/admin/setrule', 'AdminController@setRule')->name('admin.setrule');
+	Route::post('/admin/setrule', 'AdminController@saveRule')->name('admin.setrule.submit');
 
-
-		// Set rules that users will transfer with
-		Route::get('/admin/setrule', 'AdminController@setRule')->name('admin.setrule');
-		Route::post('/admin/setrule', 'AdminController@saveRule')->name('admin.setrule.submit');
-
-		// New Rule <Creati></Creati>on
-		Route::get('/admin/createrule', 'AdminController@createRule')->name('admin.createrule');
-		Route::post('/admin/createrule', 'AdminController@saveNewRule')->name('admin.setrule.submit');
+	// New Rule <Creati></Creati>on
+	Route::get('/admin/createrule', 'AdminController@createRule')->name('admin.createrule');
+	Route::post('/admin/createrule', 'AdminController@saveNewRule')->name('admin.setrule.submit');
 
 	//Route::get('/manager/setting', 'AdminController@settings');
+	
+	// admin routes
+	Route::get('/manager', 'AdminController@index');
+	Route::get('/view-accounts', 'pagesController@viewAccounts');
+	Route::get('/addaccount', 'AdminController@addaccount');
+	Route::get('/usermanagement', 'AdminController@usermanagement');
+	Route::get('/web-analytics', 'pagesController@webAnalytics');
+	Route::get('/banks', 'BanksController@banks');
+	Route::get('/success', 'pagesController@success');
+	Route::get('/failed', 'pagesController@failed');
+	Route::get('/transfer', 'pagesController@transfer');
+	Route::get('/balance', 'pagesController@balance');
 });
