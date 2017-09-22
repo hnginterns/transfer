40d2cd708481cf0abd0db7b211d1384a7271d70b
Tim <fabrobocomx@gmail.com>
2017-09-22 10:30:08 +0100

    Merge conflict

--- app/Http/Controllers/pagesController.php
+++ app/Http/Controllers/pagesController.php
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class pagesController extends Controller
 {
-<<<<<<< HEAD
 
   public function home () {
     return view('home-page');
@@ -37,39 +36,4 @@ class pagesController extends Controller
     {all the logic}
     return view('<blade name>');
   }*/
-=======
-    use AuthenticatesUsers;
-
-    protected $redirectTo = '/dashboard';
-
-
-
-    public function home () {
-      return view('home-page');
-    }
-
-    public function signin(Request $request) 
-    {
-      $data['ref'] = str_replace('http://', '', str_replace('https://', '', URL::previous()));
-      $data['host'] = str_replace('http://', '', str_replace('https://', '', $request->server('HTTP_HOST')));
-
-      return $this->showLoginForm($data);
-      //return view ('sign-in', $data);
-    }
-
-    public function balance () {
-      return view ('balance');
-    }
-
-    public function transfer () {
-      return view ('transfer');
-    }
-
-    //all other page functions can be added
-    /*
-    pubic function <function name> {
-      {all the logic}
-      return view('<blade name>');
-    }*/
->>>>>>> 69084c30af19a3c1728bb55664ad5e8a88f501a7
 }
