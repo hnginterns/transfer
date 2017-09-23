61240807efc51da251c9ea53bd33f477adb3379c
Okwu Joscelyn Kinikachi <ojk564@gmail.com>
2017-09-23 20:46:53 +0100

    Update BanksController.php

--- app/Http/Controllers/BanksController.php
+++ app/Http/Controllers/BanksController.php
@@ -15,7 +15,7 @@ class BanksController extends Controller
         //var_dump(is_curl());
 
         //die();
-        \Unirest\Request::verifyPeer(false);
+        //\Unirest\Request::verifyPeer(false);
         $headers = array('content-type' => 'application/json');
         $response = \Unirest\Request::post('https://moneywave.herokuapp.com/banks', $headers);
         $data = json_decode($response->raw_body, TRUE);
