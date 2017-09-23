4a864179145b51452b135456fcfe979ba80c3ccf
Emmanuel Joseph <jekayode@live.com>
2017-09-23 13:41:25 +0100

    Updated User Create Form

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,25 +1,25 @@
 @extends('layouts.admin')
 @section('content')
-<div class="container">
+<div class="col-sm-10">
   <form method="post" action="{{url('admin/users')}}">
     <div class="form-group row">
       {{csrf_field()}}
       <label for="lgFormGroupInput" class="col-sm-2 col-form-label col-form-label-lg">Name</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="title" name="name">
+        <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="Name" name="name">
       </div>
     </div>
     <div class="form-group row">
       <label for="smFormGroupInput" class="col-sm-2 col-form-label col-form-label-sm">Email</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="title" name="email">
+        <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="Email" name="email">
       </div>
     </div>
 
     <div class="form-group row">
       <label for="smFormGroupInput" class="col-sm-2 col-form-label col-form-label-sm">Password</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="title" name="password">
+        <input type="password" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="Password" name="password">
       </div>
     </div>
 
