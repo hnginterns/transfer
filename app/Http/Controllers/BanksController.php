60e3a8a17c80adbc34bca4216b5959c99d1a12ea
DESKTOP-E9ALBBV\profchydon <profchydon@gmail.com>
2017-09-23 13:40:55 +0100

    Modifies page navigations

--- app/Http/Controllers/BanksController.php
+++ app/Http/Controllers/BanksController.php
@@ -7,6 +7,7 @@ class BanksController extends Controller
 {
       public function banks()
     {
+        \Unirest\Request::verifyPeer(false);
         $headers = array('content-type' => 'application/json');
         $response = \Unirest\Request::post('https://moneywave.herokuapp.com/banks', $headers);
         $data = json_decode($response->raw_body, TRUE);
