6db6af9c9aad296db2777e22ebdf7e45f9d1487b
= <=>
2017-09-23 16:58:16 +0100

    removed function to test curl

--- app/Http/Controllers/BanksController.php
+++ app/Http/Controllers/BanksController.php
@@ -8,13 +8,6 @@ class BanksController extends Controller
       public function banks()
     {
 
-       /** function is_curl() {
-            return function_exists('curl_version');
-        }**/
-
-        //var_dump(is_curl());
-
-        //die();
         \Unirest\Request::verifyPeer(false);
         $headers = array('content-type' => 'application/json');
         $response = \Unirest\Request::post('https://moneywave.herokuapp.com/banks', $headers);
