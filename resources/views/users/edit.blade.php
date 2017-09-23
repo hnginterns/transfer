2a0813c480b5e966004181195531621d427e7210
Emmanuel Joseph <jekayode@live.com>
2017-09-23 15:24:27 +0100

    Admin Users Details

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -2,13 +2,13 @@
 @section('content')
 
 <div class="container">
-  <form method="post" action="{{action('CRUDController@update', $id)}}">
+  <form method="post" action="{{action('Admin\UsersController@update', $id)}}">
     <div class="form-group row">
       {{csrf_field()}}
        <input name="_method" type="hidden" value="PATCH">
       <label for="lgFormGroupInput" class="col-sm-2 col-form-label col-form-label-lg">Name</label>
       <div class="col-sm-10">
-        <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="title" name="title" value="{{$user->email}}">
+        <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="title" name="title" value="{{$user->username}}">
       </div>
     </div>
     <div class="form-group row">
