100eb193139a76aed026d2ef541c4f7e8e9102b2
Emmanuel Joseph <jekayode@live.com>
2017-09-23 23:33:52 +0100

    Modify Add User Form

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -20,6 +20,20 @@
     </div>
 
     <div class="form-group row">
+      <label for="email" class="col-sm-2 col-form-label col-form-label-sm">Firstname</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="Firstname" name="first_name"  value="{{$user->first_name}">
+      </div>
+    </div>
+
+    <div class="form-group row">
+      <label for="email" class="col-sm-2 col-form-label col-form-label-sm">Lastname</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="Lastname" name="last_name"  value="{{$user->last_name}}">
+      </div>
+    </div>
+
+    <div class="form-group row">
       <label for="email" class="col-sm-2 col-form-label col-form-label-sm">Account Number</label>
       <div class="col-sm-10">
         <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="Account Number" name="account_number" value="{{$user->account_number}}">
