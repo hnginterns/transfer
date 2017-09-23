3b1ca2f45a964209807a75397a3f6d901ef12e66
Bosun Egberinde <bosunski@gmail.com>
2017-09-23 16:42:53 +0100

    Fixing cURL issue

--- app/Http/Controllers/BanksController.php
+++ app/Http/Controllers/BanksController.php
@@ -12,9 +12,9 @@ class BanksController extends Controller
             return function_exists('curl_version');
         }
 
-        var_dump(is_curl());
+        //var_dump(is_curl());
 
-        die();
+        //die();
         \Unirest\Request::verifyPeer(false);
         $headers = array('content-type' => 'application/json');
         $response = \Unirest\Request::post('https://moneywave.herokuapp.com/banks', $headers);
