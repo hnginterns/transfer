254cd73a05c2032aa012b2ea65ceca48349c4217
DESKTOP-E9ALBBV\profchydon <profchydon@gmail.com>
2017-09-24 02:50:13 +0100

    Linked manage wallet page

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -45,7 +45,7 @@
        <li>
         <a href="{{ url('/admin/setrule') }}><i class="fa fa-group"></i> <span>Set Rules</span></a>
       </li>
-      <li><a href="{{ url('/admin') }}"><i class="fa fa-briefcase"></i> <span>Manage Wallet</span></a></li>
+      <li><a href="{{ url('/admin/managewallet') }}"><i class="fa fa-briefcase"></i> <span>Manage Wallet</span></a></li>
       <li><a href="#"><i class="fa fa-dollar"></i> <span>Currencies</span></a></li>
       <li><a href="{{ url('/web-analytics') }}">Transaction Analytics</a></li>
       <li><a href="#"><i class="fa fa-eyedropper"></i> <span>Draft</span></a></li>
