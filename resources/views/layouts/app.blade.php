850010993a0a90feb1b391c51dc0ddfd7eda2752
PlushDeveloper <oreeboy@gmail.com>
2017-09-24 19:25:10 +0100

    added route to admin login in login page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,9 @@
               <li><a href="#">Demo</a></li>
 
               @if(Auth::guest())
-                <li id="sign-in"><a href="/signin">SIGN IN</a></li>     
+                <li id="sign-in"><a href="/signin">SIGN IN</a></li>
+                @elseif(route(login))
+                <li id="sign-in"><a href="{{ route('admin.login') }}">ADMIN SIGN IN</a></li>     
               @else
                 <li id="sign-in">
                                         <a href="{{ route('logout') }}"
