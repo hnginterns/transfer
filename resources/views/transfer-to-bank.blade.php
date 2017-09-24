a593a220c63025b8879017951811dffece8efc87
= <=>
2017-09-24 01:01:18 +0100

    -m

--- resources/views/transfer-to-bank.blade.php
+++ resources/views/transfer-to-bank.blade.php
@@ -546,7 +546,7 @@
                         </div>
                         <div class="col col-lg-6 form-holder">
                           <div class="form-group">
-                            <input type="text" class="form-control cus-input" id="reference" placeholder="reference">
+                            <input type="text" class="form-control cus-input" id="amount" placeholder="reference">
                           </div>
                         </div>
                       </div>
