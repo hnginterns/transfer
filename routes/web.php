6a795addcee204f0dc8facd9f087a3fa40e96aa6
= <=>
2017-09-24 16:28:31 +0100

    a

--- routes/web.php
+++ routes/web.php
@@ -51,7 +51,9 @@ Route::group(['middleware' => 'auth'], function() {
 	Route::get('/wallet-view', 'pagesController@viewWallet')->name('wallet');
 	Route::get('/banks', 'BanksController@banks');
 	Route::get('/success', 'pagesController@success');
-	Route::get('/failed', 'pagesController@failed');
+	Route::get('/failed', function() {
+		return redirect('failed');
+	});
 	Route::get('/transfer', 'pagesController@transfer');
 	Route::get('/balance', 'pagesController@balance');
 	Route::get('/ravepay', 'RavepayController@index');
