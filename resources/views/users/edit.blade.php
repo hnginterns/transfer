81a3d83ec4bdacd9a578851f05b5fba62ae2788a
Okwu Joscelyn Kinikachi <ojk564@gmail.com>
2017-09-25 21:16:37 +0100

    Update edit.blade.php

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -19,7 +19,7 @@
       </div>
     </div>
 
-<div class="form-group row">
+    <div class="form-group row">
       <label for="email" class="col-sm-2 col-form-label col-form-label-sm">Firstname</label>
       <div class="col-sm-10">
         <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="Email" name="first_name" value="{{$user->first_name}}">
@@ -30,14 +30,6 @@
       <label for="email" class="col-sm-2 col-form-label col-form-label-sm">Lastname</label>
       <div class="col-sm-10">
         <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="Email" name="last_name" value="{{$user->last_name}}">
-      </div>
-    </div>
-
-
-    <div class="form-group row">
-      <label for="email" class="col-sm-2 col-form-label col-form-label-sm">Account Number</label>
-      <div class="col-sm-10">
-        <input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="Account Number" name="account_number" value="{{$user->account_number}}">
       </div>
     </div>
 
