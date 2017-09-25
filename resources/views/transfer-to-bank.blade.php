ea4ab69d529c9464c7b974094f7f95fb221c963b
Franklin Nwanze <nwanzechuka1993@gmail.com>
2017-09-25 11:16:51 +0100

    added bank transfer validation and transfer

--- resources/views/transfer-to-bank.blade.php
+++ resources/views/transfer-to-bank.blade.php
@@ -497,14 +497,16 @@
                   {{csrf_field()}}
                     <div class="row">
                       <div class="col col-lg-6 form-holder">
-                        <select class="form-control cus-input" name="benefi">
+                        <select class="form-control cus-input" name="beneficiary_id">
                           <option>Select Beneficiary</option>
-                            <option value="">Bene 1</option>
+                            @foreach($beneficiary as $key => $beneficiaries)
+                              <option value="{{$beneficiaries->id}}">{{$beneficiaries->name}}</option>
+                            @endforeach
                         </select>
                         </div>
                       <div class="col col-lg-6 form-holder">
                           <div class="form-group" style="margin: 30px 0;">
-                              <input type="text" class="form-control cus-input" name="senderName" id="senderName" placeholder="Sender's Name">
+                              <input type="text" class="form-control cus-input" name="sender_name" id="senderName" placeholder="Sender's Name">
                           </div>
                       </div>
                   </div>
