6f6ff6cba1c9b48cff03b8d4beb19a07ebc10d01
Okwu Joscelyn Kinikachi <ojk564@gmail.com>
2017-09-25 22:38:51 +0100

    Update WalletController.php

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -97,7 +97,7 @@ class WalletController extends Controller
 
                }
                print_r($response_arr);
-               return response()->json(['status' => 'failed']);
+               return response()->json(['status' => 'failed', 'msg' => $response_arr['message']);
             }
 
             public function transferAccount(Request $request){
