7472e08af85819184de635eeb72b119fc254a186
Okwu Joscelyn Kinikachi <ojk564@gmail.com>
2017-09-25 22:49:12 +0100

    Update WalletController.php

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -69,6 +69,8 @@ class WalletController extends Controller
     }
 
     public function transfer(Request $request, WalletTransaction $transaction){
+                $lock_code = Wallet::where('uuid', Auth::user()->id)->get();
+                print_r($lock_code);
                 $token = $this->getToken();
                 $headers = array('content-type' => 'application/json', 'Authorization' => $token);
                 $query = array(
