a905bd0db0ed702131efbd1ca06aa64a64d3d0f7
Bosun Egberinde <bosunski@gmail.com>
2017-09-26 01:43:22 +0100

    Updated Trasfer to bank Logic

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -153,19 +153,20 @@ class WalletController extends Controller
                 $validator = $this->validateBeneficiary($request->all());
 
                 if ($validator->fails()) {
-                    //dd($request);
                     $messages = $validator->messages()->toArray();
                     Session::flash('messages', $this->formatMessages($messages, 'error'));
                     return redirect()->to(URL::previous())->withInput();
                 } else {
 
                         $beneficiary = Beneficiary::where('id', '=', $request->beneficiary_id)
-                                                    ->get();                     
+                                                    ->get();
+                        // We need to get the loack code of a wallet in order to make the transfer.
+                        $wallet_data = Wallet::where('wallet_name', $request->wallet_name)->get();                     
                         if(!empty($beneficiary)){
                             $token = $this->getToken();
                             $headers = array('content-type' => 'application/json', 'Authorization' => $token);
                             $query = array(
-                            "lock"=>$request->lock_code,
+                            "lock"=>$wallet_data->lock_code,
                             "amount"=>$request->amount,
                             "bankcode"=>$beneficiary[0]->bank_id,
                             "accountNumber"=>$beneficiary[0]->account_number,
