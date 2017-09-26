d8895ac5f673d04e2eacbbcbffd4fcc388a7734e
stephenjude <stephenjudesuccess@gmail.com>
2017-09-26 22:28:43 +0100

    update

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -37,7 +37,8 @@
         <a href="{{ route('logout') }}"
             onclick="event.preventDefault();
                      document.getElementById('logout-form').submit();">
-            Logout
+                     <i class="fa fa-sign-out" aria-hidden="true"></i>
+                      <span> Logout</span>
         </a>
 
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
