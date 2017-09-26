a0ed67fa1eae9474b9968f31c4d882712187529d
stephenjude <stephenjudesuccess@gmail.com>
2017-09-26 01:15:53 +0100

    update

--- resources/views/admin/walletdetails.blade.php
+++ resources/views/admin/walletdetails.blade.php
@@ -116,19 +116,19 @@
             
             </div>           
           </div>
-          <div class="col-sm-12"> 
-            <div class="col-sm-2">
-              <a type="submit" class="btn btn-block btn-info" >Fund Wallet</a>
-            </div>
-            <div class="col-sm-2">
-              <a type="submit" class="btn btn-block btn-danger">Delete Wallet</a>       
-            </div>      
-            <div class="col-sm-6"></div>
-          </div> 
+            
     </div>
-
-    
-
+    <div class="row">
+      <div class="col-sm-12"> 
+        <div class="col-sm-2">
+          <a type="submit" class="btn btn-block btn-info" >Fund Wallet</a>
+        </div>
+        <div class="col-sm-2">
+          <a type="submit" class="btn btn-block btn-danger">Delete Wallet</a>       
+        </div>      
+        <div class="col-sm-6"></div>
+      </div>
+    </div>
 </div>
 
 @endsection
