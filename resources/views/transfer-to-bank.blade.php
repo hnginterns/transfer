924c07c07676ff2675a10dd9413d2a996a41c5a0
Okwu Joscelyn Kinikachi <ojk564@gmail.com>
2017-09-27 22:48:22 +0100

    Update transfer-to-bank.blade.php

--- resources/views/transfer-to-bank.blade.php
+++ resources/views/transfer-to-bank.blade.php
@@ -471,15 +471,9 @@
           </li>
 
           <li>
-          <a href="{{ route('logout') }}"
-              onclick="event.preventDefault();
-                       document.getElementById('logout-form').submit();">
+          <a href="{{ url('/logout') }}">
               Logout
           </a>
-
-          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-              {{ csrf_field() }}
-          </form>
         </li>
         </ul>
       </div>
