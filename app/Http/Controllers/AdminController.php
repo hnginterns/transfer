3f8ce829c922c2993818e7b5c064c30db7ca99d1
Olatunbosun Gabriel <bosunski@gmail.com>
2017-09-28 10:54:11 -0700

    Integrated Wallet Archiving

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -281,7 +281,7 @@ class AdminController extends WalletController
     public function archiveWallet($id) {
       $wallet = Wallet::findOrFail($id);
 
-      Wallet::where('id', $id)->update(['archived', => 0]);
+      Wallet::where('id', $id)->update(['archived' => 0]);
 
       return redirect('admin/viewwallet/{{ '.$id.' }}')->with('message', 'Wallet Archived successfully.')
     }
