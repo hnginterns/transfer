a0a5138ef2ad2bb45628394283f45501152afe9e
stephenjude <stephenjudesuccess@gmail.com>
2017-10-02 15:57:43 +0100

    fix

--- app/Http/Controllers/pagesController.php
+++ app/Http/Controllers/pagesController.php
@@ -84,7 +84,6 @@ class pagesController extends Controller
   {
     $beneficiary = Beneficiary::all();
     $wallets = Wallet::where('uuid', '=', Auth::user()->id)->get();
-    $data = ['dgd' => 'shshs'];
     if (!empty($wallet)) {
       //$wallet = $wallet[0];
 
