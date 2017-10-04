62271846a683cacebadd5fa0ec9e8db0c4d4d2a2
stephenjude <stephenjudesuccess@gmail.com>
2017-10-04 15:45:41 +0100

    fix

--- resources/views/to-main-wallet.blade.php
+++ resources/views/to-main-wallet.blade.php
@@ -13,21 +13,49 @@
                   <form class="input-form" action="/transferAccount" method="POST">
                   {{csrf_field()}}
 
-                    <div class="form-group">
-                      
-                    </div>
-                      
-                 
-                          <div class="form-group">
-                          </div>
+                  <div class="form-group">  
+                    <input type="text" name="fname"  class="form-control input-defaulted" placeholder="First Name">                       
+                  </div> 
 
-                            <div class="form-group">
-                              
-                            </div>
+                  <div class="form-group">  
+                  <input type="text" name="lname"  class="form-control" placeholder="Last Name">  
+                    </div> 
+                     
+                  <div class="form-group">      
+                  <input type="text" name="phone"  class="form-control" placeholder="Phone Number"> 
+                    </div> 
+                     
+                  <div class="form-group">       
+                  <input type="email" name="emailaddr"  class="form-control" placeholder="Email Address"> 
+                    </div> 
+                     
+                  <div class="form-group">       
+                  <input type="number" name="card_no"  class="form-control" placeholder="Card No.">  
+                    </div> 
+                     
+                  <div class="form-group">      
+                  <input type="number" name="cvv"  class="form-control" placeholder="CVV">          
+                    </div> 
+                     
+                  <div class="form-group">         
+                  <input type="number" name="expiry_year" class="form-control " placeholder="Expiry Year">
+                    </div> 
+                     
+                  <div class="form-group"> 
+                  <input type="number" name="expiry_month" class="form-control" placeholder="Expiry Month">  
+                  </div> 
+                     
+                  <div class="form-group"> 
+                  <input type="number" name="pin" class="form-control" placeholder="PIN">  
+                  </div> 
+                     
+                  <div class="form-group"> 
+                  <input type="number" name="amount" class="form-control" placeholder="Amount">  </div> 
+                  </div>
                             
-                        <div class="form-group center-block">    
-                          <button id="" type="submit" class="btn btn-primary center-block">Transfer</button>
-                        </div>
+                  <div class="form-group center-block">    
+                    <button id="" type="submit" class="btn btn-primary center-block">Transfer</button>
+                  </div>
 
 
                   </form>
