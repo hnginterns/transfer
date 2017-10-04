cebe3b993725a0bf1a9cec8c92c26c2f1db4dead
Michael Okoh <ordrizzy@gmail.com>
2017-10-04 21:22:32 +0100

    Made all controller PSR-2 compliant

--- app/Http/Controllers/transactionController.php
+++ app/Http/Controllers/transactionController.php
@@ -7,11 +7,13 @@ use App\Http\Controllers\Controller;
 
 class transactionController extends Controller
 {
-    public function toBankSuccess(){
+    public function toBankSuccess()
+    {
         return view('/transactionsuccess');
     }
 
-    public function toBankFailed(){
+    public function toBankFailed()
+    {
         return view('/transfer-to-bank');
     }
 }
