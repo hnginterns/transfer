f77da582f86115796cd57d35fa523fa87aebe8fb
Emmanuel Joseph <jekayode@live.com>
2017-10-06 22:39:14 +0100

    Rave amount pick input

--- resources/views/ravepay.blade.php
+++ resources/views/ravepay.blade.php
@@ -60,7 +60,7 @@
           }
         });
         var PBFKey = "FLWPUBK-47d14cd9504c1b0c54b439e1be251fcf-X";
-        var amount = document.getElementById("amount");;
+        var amount = document.getElementById("amount");
     
         // getpaidSetup is Rave's inline script function. it holds the payment data to pass to Rave.
         getpaidSetup({
