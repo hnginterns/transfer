bbdd0da9161426b34c05e94dae6862c2baa82b8f
Emmanuel Joseph <jekayode@live.com>
2017-10-06 22:04:01 +0100

    Wallet Beneficiary Display

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -26,6 +26,8 @@
         </div>
 
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        @include('partials.messages')
+
         @yield('content')
         </div>
 
