acb4fea13938f9c0e229959e8c4330e55b46746c
= <=>
2017-10-06 04:00:19 +0100

    a

--- resources/views/admin/fundwallet.blade.php
+++ resources/views/admin/fundwallet.blade.php
@@ -109,7 +109,7 @@
           <h4 class="modal-title">Otp</h4>
         </div>
         <div class="modal-body">
-          <p>{{$transMsg}}</p>
+          <p>{{session('status')}}</p>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
