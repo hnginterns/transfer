a232e5cb68cba8fa24804e868675aaf3189d268b
= <=>
2017-10-06 03:57:22 +0100

    a

--- resources/views/admin/fundwallet.blade.php
+++ resources/views/admin/fundwallet.blade.php
@@ -107,10 +107,10 @@
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
+          <h4 class="modal-title">Otp</h4>
         </div>
         <div class="modal-body">
-          <p>Some text in the modal.</p>
+          <p>{{$transMsg}}</p>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
