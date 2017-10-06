544e18a0acac2a7357b1219c21ee6ac3f0b9b617
= <=>
2017-10-06 03:55:16 +0100

    a

--- resources/views/admin/fundwallet.blade.php
+++ resources/views/admin/fundwallet.blade.php
@@ -124,3 +124,4 @@
 
 @endsection
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -78,6 +78,7 @@
 <!-- ./wrapper -->
 
 <<!-- jQuery 3 -->
+
 <script src="https://adminlte.io/themes/AdminLTE/bower_components/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
 <script src="https://adminlte.io/themes/AdminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
