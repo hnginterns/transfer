932de0da4392212accfe88ea4181b2ef8b84c9d6
= <=>
2017-10-06 02:30:18 +0100

    a

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -77,7 +77,7 @@ class WalletController extends Controller
             $response = $response['transfer']['meta'];
             $response = json_decode($response, TRUE);
             $transRef = $response['processor']['transactionreference'];
-            return back()->with('transactionRef', $transRef);
+            return redirect('otp');
             $headers = array('content-type' => 'application/json');
             $query = array('transactionRef'=>$transRef,'otp' => '123456'); // array contains all the required parameters;
             $body = \Unirest\Request\Body::json($query);
