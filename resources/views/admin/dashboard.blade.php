8e38351514299d75dd69eff7423ed6c6db06fac5
= <=>
2017-10-06 08:33:11 +0100

    a

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -13,7 +13,8 @@ $totalwallets = count($wallets);
 
 	<section class="content">
     @if (session('status'))
-    <div class="alert alert-warning alert-dismissible" role="alert">
+    <div class="alert alert-success alert-dismissible" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         {{ session('status') }}
     </div>
   @endif
