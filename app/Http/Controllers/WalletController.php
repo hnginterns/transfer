4fdb9173c847ff9db3492731df659923862d3c3e
= <=>
2017-10-07 18:09:42 +0100

    a

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -269,8 +269,8 @@ class WalletController extends Controller
         //die();
         foreach($walletBalance as $wallets)
         {
-            if($wallets['uref'] == 'eb475a036c') {
-                Wallet::where('wallet_code', 'eb475a036c')
+            if($wallets['uref'] == 'c6e1e5d10e') {
+                Wallet::where('wallet_code', 'c6e1e5d10e')
                         ->update(['balance'=> $wallets['balance']]);
             }
           //return view('walletBalance', compact('walletBalance'));
