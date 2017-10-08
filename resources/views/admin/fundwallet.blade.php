2dcf3628cb08178c747d80c59feb5bfec0ac4125
= <=>
2017-10-08 14:26:24 +0100

    modified funding of main wallet

--- resources/views/admin/fundwallet.blade.php
+++ resources/views/admin/fundwallet.blade.php
@@ -79,6 +79,7 @@
         <div class="single-beneficiary-holder col-md-6">
             <div class="inner-holder">
                 <div class="flex">
+                  <h4>Fund Main Wallet</h4>
                 <h5 class="beneficiary-name"><b>Fund: </b> Wallet Name</h5>
                 <span class="float"><a href="transaction-history"><button type="submit" class="btn btn-success btn-xs">view history</button></a></span>
               </div>
