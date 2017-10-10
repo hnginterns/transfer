2f9a5e9016bed9d159ee49b689f143ac56d0233c
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-10 22:11:50 +0100

    Update features.blade.php

--- resources/views/pages/features.blade.php
+++ resources/views/pages/features.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.pages')
+@extends('layouts.page')
 @section('main-text', 'Features')
 @section('sub-text', 'TransferRules lets you receive payments locally and globally with no hassles and zero set up fees')
 @section('content')
