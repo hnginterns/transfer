52f2ab067dc4157a5f6793a2e23044d6cf6b750e
psalmfill <samfield4sure@gmail.com>
2017-10-10 20:38:07 +0100

    Update how.blade.php

--- resources/views/pages/how.blade.php
+++ resources/views/pages/how.blade.php
@@ -4,28 +4,33 @@
 @section('content')
 
 <!-- SECTION ONE BEGINS -->
-<div class="panel-group" id="accordion">
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
-        What is TransferRule?</a>
-      </h4>
+  <div id="accordion" role="tablist" aria-multiselectable="true">
+  <div class="card">
+    <div class="card-header" role="tab" id="headingOne">
+      <h5 class="mb-0">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          What is TransferRule?
+        </a>
+      </h5>
     </div>
-    <div id="collapse1" class="panel-collapse collapse in">
-    <div class="panel-body">TransferRule is a secure online service that enables any user to instantly buy mobile credit, data top-up for a prepaid Mobile Phone in any part of the country. Our customers use transferrules.com to buy Mobile Airtime for either themselves, friends and family members.
-</div>
+
+    <div id="collapseOne" class="collapse show" role="tabpanel" aria-labelledby="headingOne">
+      <div class="card-block">
+        <p>TransferRule is a secure online service that enables any user to instantly buy mobile credit, data top-up for a prepaid Mobile Phone in any part of the country. Our customers use transferrules.com to buy Mobile Airtime for either themselves, friends and family members.</p>
+      </div>
     </div>
   </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
-        How do I register to use TransferRule?</a>
-      </h4>
+  <div class="card">
+    <div class="card-header" role="tab" id="headingTwo">
+      <h5 class="mb-0">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+          How do I register to use TransferRule?
+        </a>
+      </h5>
     </div>
-    <div id="collapse2" class="panel-collapse collapse">
-      <div class="panel-body">
+
+    <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo">
+      <div class="card-block">
         <ul>
           <li>Create an account at transferrules.com.</li>
           <li>Select ‘Register’ at the Top Left of the Page</li>
@@ -34,40 +39,44 @@
           <li>A Confirmation Code will be sent directly to your email.</li>
           <li>Click on the code to verify your email address and continue to setting up your account</li>
         </ul>
-    </div>
+      </div>
     </div>
   </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
-        How do I send Prepaid Mobile Credit to an existing customer?</a>
-      </h4>
+  <div class="card">
+    <div class="card-header" role="tab" id="headingThree">
+      <h5 class="mb-0">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+          How do I send Prepaid Mobile Credit to an existing customer?
+        </a>
+      </h5>
     </div>
-    <div id="collapse3" class="panel-collapse collapse">
-      <div class="panel-body">
-    <ul>
-      <li>Login to transferrules.com.</li>
-      <li>Select a number from the list of numbers registered under your account</li>
-      <li>Select the Top Up recharge amount.</li>
-      <li>Click the Top Up button.</li>
-      <li>Carefully review the order and complete payment using our secure online payment process.</li>
-      <li>A confirmation message will appear on your screen with details on your completed transaction.</li>
-      <li>A confirmation email will be sent to your email address.</li>
-    </ul>
+
+    <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree">
+      <div class="card-block">
+        <ul>
+          <li>Login to transferrules.com.</li>
+          <li>Select a number from the list of numbers registered under your account</li>
+          <li>Select the Top Up recharge amount.</li>
+          <li>Click the Top Up button.</li>
+          <li>Carefully review the order and complete payment using our secure online payment process.</li>
+          <li>A confirmation message will appear on your screen with details on your completed transaction.</li>
+          <li>A confirmation email will be sent to your email address.</li>
+        </ul>
     <p>NOTE: Recipients will also receive a transaction confirmation SMS message from their Mobile operator.</p>
-</div>
+      </div>
     </div>
   </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">
-        How do I send Data Plan to an existing customer?</a>
-      </h4>
+  <div class="card">
+    <div class="card-header" role="tab" id="headingFour">
+      <h5 class="mb-0">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+          How do I send Data Plan to an existing customer?
+        </a>
+      </h5>
     </div>
-    <div id="collapse4" class="panel-collapse collapse">
-      <div class="panel-body">
+
+    <div id="collapseFour" class="collapse" role="tabpanel" aria-labelledby="headingFour">
+      <div class="card-block">
         <p>Login to transferrules.com.</p>
         <ul>
           <li>Select a number from the list of numbers registered under your account.</li>
@@ -76,34 +85,22 @@
           <li>A confirmation message will appear on your screen with details on your completed transaction.</li>
           <li>A confirmation email will be sent to your email address.</li>
         </ul>
-  <p>NOTE: Recipients will also receive a transaction confirmation SMS message from their Mobile operator.</p>
-</div>
+        <p>NOTE: Recipients will also receive a transaction confirmation SMS message from their Mobile operator.</p>
+      </div>
     </div>
   </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse5">
-        Collapsible Group 1</a>
-      </h4>
+    <div class="card">
+    <div class="card-header" role="tab" id="headingFive">
+      <h5 class="mb-0">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
+          What should I do if have forgotten my password?
+        </a>
+      </h5>
     </div>
-    <div id="collapse5" class="panel-collapse collapse">
-      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.</div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse6">
-        What should I do if have forgotten my password?</a>
-      </h4>
-    </div>
-    <div id="collapse6" class="panel-collapse collapse">
-      <div class="panel-body">
-       <p>Please take the following steps to reset your account password:</p>
+
+    <div id="collapseFive" class="collapse" role="tabpanel" aria-labelledby="headingFive">
+      <div class="card-block">
+        <p>Please take the following steps to reset your account password:</p>
         <p>Login to transferrules.com.</p>
         <ul>
           <li>Input your email address</li>
@@ -112,89 +109,117 @@
           <li>A message will then be sent to your email address with a link to reset the account password.</li>
           <li>Check your email and follow the instructions in the email to reset your account password and proceed further.</li>
         </ul>
-</div>
+      </div>
     </div>
   </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse7">
-        How do I credit my Top-Up Wallet?</a>
-      </h4>
+    <div class="card">
+    <div class="card-header" role="tab" id="headingSix">
+      <h5 class="mb-0">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseSix" aria-expanded="true" aria-controls="collapseSix">
+          How do I credit my Top-Up Wallet?
+        </a>
+      </h5>
     </div>
-    <div id="collapse7" class="panel-collapse collapse ">
-      <div class="panel-body">
-      <p>Login to transferrules.com.</p>
-      <ul>
-         <li>Select the amount you wish to Refill Top up wallet.</li>
-        <li>Click the TopUp button.</li>
-        <li>Carefully review and confirm your request.</li>
-        <li>A confirmation email will be sent to your email address.</li>
-      </ul>
-</div>
-    </div>
-  </div>
-<div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse8">
-        Can I send Top Up to more than one mobile number?</a>
-      </h4>
-    </div>
-    <div id="collapse8" class="panel-collapse collapse ">
-      <div class="panel-body">
-      <p>Yes, you can send credit or data top-up to more than one mobile number.</p>
-</div>
+
+    <div id="collapseSix" class="collapse" role="tabpanel" aria-labelledby="headingSix">
+      <div class="card-block">
+        <p>Login to transferrules.com.</p>
+        <ul>
+           <li>Select the amount you wish to Refill Top up wallet.</li>
+          <li>Click the TopUp button.</li>
+          <li>Carefully review and confirm your request.</li>
+          <li>A confirmation email will be sent to your email address.</li>
+        </ul>
+      </div>
     </div>
   </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse9">
-        What is the cost to send a Top Up?</a>
-      </h4>
+    <div class="card">
+    <div class="card-header" role="tab" id="headingSeven">
+      <h5 class="mb-0">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseSeven" aria-expanded="true" aria-controls="collapseSeven">
+           What is the cost to send a Top Up?
+        </a>
+      </h5>
     </div>
-    <div id="collapse9" class="panel-collapse collapse ">
-      <div class="panel-body">
-      <p>The cost of a Top Up transaction is based upon the denomination amount selected. During the checkout process the total amount due will be indicated. This amount includes the details of any additional charges (such as taxes or SMS fees) that affect the ultimate Top Up recharge amount that will be received by the transaction recipient.</p>
-</div>
-    </div>
-  </div>
-   <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse10">
-       What happens if I Top Up an invalid mobile number?</a>
-      </h4>
-    </div>
-    <div id="collapse10" class="panel-collapse collapse ">
-      <div class="panel-body">
-      <p>If the Mobile Number is invalid and does not exist, the Top Up transaction will not be carried out and you will not be charged for the transaction.</p>
-</div>
+
+    <div id="collapseSeven" class="collapse" role="tabpanel" aria-labelledby="headingSeven">
+      <div class="card-block">
+        <p>The cost of a Top Up transaction is based upon the denomination amount selected. During the checkout process the total amount due will be indicated. This amount includes the details of any additional charges (such as taxes or SMS fees) that affect the ultimate Top Up recharge amount that will be received by the transaction recipient.</p>
+      </div>
     </div>
   </div>
-   <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse11">
-        What happens when I am unable to top up online and the page gives an error?</a>
-      </h4>
+    <div class="card">
+    <div class="card-header" role="tab" id="headingEight">
+      <h5 class="mb-0">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseEight" aria-expanded="true" aria-controls="collapseEight">
+         What happens if I Top Up an invalid mobile number?
+        </a>
+      </h5>
     </div>
-    <div id="collapse11" class="panel-collapse collapse ">
-      <div class="panel-body">
-      <p>Our website is best enjoyed while using Internet Explorer or Google Chrome. Please avoid using other web browsers like Firefox, Opera or UC browser. If the problem still persists, please raise a complaint with our Customer Service team by calling 000111 from your mobile number or send a mail via finance@hotels.ng </p>
-</div>
+
+    <div id="collapseEight" class="collapse" role="tabpanel" aria-labelledby="headingEight">
+      <div class="card-block">
+       <p>If the Mobile Number is invalid and does not exist, the Top Up transaction will not be carried out and you will not be charged for the transaction.</p>
+      </div>
     </div>
   </div>
-   <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse12">
-       How can i get my blocked account unblocked?</a>
-      </h4>
+    <div class="card">
+    <div class="card-header" role="tab" id="headingNine">
+      <h5 class="mb-0">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseNine" aria-expanded="true" aria-controls="collapseNine">
+          What happens when I am unable to top up online and the page gives an error?
+        </a>
+      </h5>
     </div>
-    <div id="collapse12" class="panel-collapse collapse ">
-      <div class="panel-body">
+
+    <div id="collapseNine" class="collapse" role="tabpanel" aria-labelledby="headingNine">
+      <div class="card-block">
+        <p>Our website is best enjoyed while using Internet Explorer or Google Chrome. Please avoid using other web browsers like Firefox, Opera or UC browser. If the problem still persists, please raise a complaint with our Customer Service team by calling 000111 from your mobile number or send a mail via finance@hotels.ng </p>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" role="tab" id="headingTen">
+      <h5 class="mb-0">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseTen">
+          ow can i get my blocked account unblocked?
+        </a>
+      </h5>
+    </div>
+
+    <div id="collapseTen" class="collapse show" role="tabpanel" aria-labelledby="headingTen">
+      <div class="card-block">
+        <p>Our website is best enjoyed while using Internet Explorer or Google Chrome. Please avoid using other web browsers like Firefox, Opera or UC browser. If the problem still persists, please raise a complaint with our Customer Service team by calling 000111 from your mobile number or send a mail via finance@hotels.ng </p>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" role="tab" id="headingTen">
+      <h5 class="mb-0">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+         How long will it take for my account to be reactivated?
+        </a>
+      </h5>
+    </div>
+
+    <div id="collapseTen" class="collapse show" role="tabpanel" aria-labelledby="headingTen">
+      <div class="card-block">
+        <p>If you use the activation form, the reactivation will be within the next 48 hours of submission, if it by email application , the activation process will be within the next 72 hours of submission.</p>
+        <p>You will receive a reply on your email from us regarding the reactivation process.</p>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" role="tab" id="headingEleven">
+      <h5 class="mb-0">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseEleven">
+          His there an eTopUpOnline.com smartphone app?
+        </a>
+      </h5>
+    </div>
+
+    <div id="collapseEleven" class="collapse show" role="tabpanel" aria-labelledby="headingEleven">
+      <div class="card-block">
         <p>To get your account unblocked, you will need to send and email to the admin at admin@transfer.hng.fun stating the reason that led to your account being blocked, also why you want it to be reactivated.<p>
         <p>OR</p> 
         <ul>
@@ -203,36 +228,22 @@
           <li>Fill in the reactivation request form.</li>
           <li>Submit</li>
         </ul>
-</div>
+      </div>
     </div>
   </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse13">
-       HHow long will it take for my account to be reactivated?</a>
-      </h4>
+  <div class="card">
+    <div class="card-header" role="tab" id="headingTwelve">
+      <h5 class="mb-0">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseTwelve">
+          What happens when I am unable to top up online and the page gives an error?
+        </a>
+      </h5>
     </div>
-    <div id="collapse13" class="panel-collapse collapse ">
-      <div class="panel-body">
-        <p>f you use the activation form, the reactivation will be within the next 48 hours of submission, if it by email application , the activation process will be within the next 72 hours of submission.
-You will receive a reply on your email from us regarding the reactivation process.</p>
-        </ul>
-</div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse14">
-       HIs there an eTopUpOnline.com smartphone app?</a>
-      </h4>
-    </div>
-    <div id="collapse14" class="panel-collapse collapse ">
-      <div class="panel-body">
-        <p>Mobile applications are currently under development for each of Android and iOS (Apple) smartphone devices. Additionally, transferrules.com. is already optimized for mobile devices in order to provide a positive user experience.</p>
-</div>
+
+    <div id="collapseTwelve" class="collapse show" role="tabpanel" aria-labelledby="headingTwelve">
+      <div class="card-block">
+        p>Mobile applications are currently under development for each of Android and iOS (Apple) smartphone devices. Additionally, transferrules.com. is already optimized for mobile devices in order to provide a positive user experience.</p>
+      </div>
     </div>
   </div>
-</div>
 @endsection
