dde69c74b3738e84f7e20bcec11136c42d6878ed
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-10 17:12:31 +0100

    Update features.blade.php

--- resources/views/pages/features.blade.php
+++ resources/views/pages/features.blade.php
@@ -8,23 +8,23 @@ Our custom built Transfer Rules is built with the aim to connect your business t
 Crisp and clear UX designs and excellent workflow serves to foster customer intimacy and user loyalty.</center><p>
 
 <h3>WHAT FEATURE DO WE OFFER?</h3><br>
-1. Admin Interface <br>
-2. Credit Score <br>
-3. Personal Finance <br>
-4. Account tracking <br>
-5. User Interface <br>
-6. Multiple Wallet support <br>
-7. Wallet to Wallet Transaction <br>
-8. Wallet to Account Transactions <br>
-9. Account Balance check <br>
-10.Detailed Transaction History <br>
-11.Data Protection <br>
-12.Robust Financial Statistics and custom reports <br>
-13.Credit/Data Top-up <br>
-14.Bulk Sms Integration <br>
-15.Live Chat <br>
-16.Secured and Reliable Payment method <br>
-17.Easy usage with fewer clicks to make payments<br>
+1.      Admin Interface <br>
+2.      Credit Score <br>
+3.      Personal Finance <br>
+4.      Account tracking <br>
+5.      User Interface <br>
+6.      Multiple Wallet support <br>
+7.      Wallet to Wallet Transaction <br>
+8.      Wallet to Account Transactions <br>
+9.      Account Balance check <br>
+10.     Detailed Transaction History <br>
+11.     Data Protection <br>
+12.     Robust Financial Statistics and custom reports <br>
+13.     Credit/Data Top-up <br>
+14.     Bulk Sms Integration <br>
+15.     Live Chat <br>
+16.     Secured and Reliable Payment method <br>
+17.     Easy usage with fewer clicks to make payments<br>
 <br>
  
 When it comes to financial app development as this, our priority lies on security, clean performance and long term viability
