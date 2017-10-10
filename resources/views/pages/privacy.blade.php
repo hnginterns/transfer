65a45ce55e2cee785fc3fc1c8d8b7a17eb10903f
psalmfill <samfield4sure@gmail.com>
2017-10-10 17:10:10 +0100

    Update privacy.blade.php

--- resources/views/pages/privacy.blade.php
+++ resources/views/pages/privacy.blade.php
@@ -5,7 +5,7 @@
 
 
          <p> TransferRule  is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using this website, then you can be assured that it will only be used in accordance with this privacy statement.
-TransferRule  may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes. This policy is effective from [2017.</p>
+TransferRule  may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes. This policy is effective from 10/10/2017.</p>
 <h2>What we collect</h2>
 <p>We may collect the following information:</p>
 <ul>
