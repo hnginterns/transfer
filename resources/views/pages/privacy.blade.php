c09de657bf8f0cf8e9445de50b379cd904a5558c
Emmanuel Joseph <jekayode@live.com>
2017-10-10 16:08:54 +0100

    Contents Layut Update

--- resources/views/pages/privacy.blade.php
+++ resources/views/pages/privacy.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
 
-        <h3>Privacy </h3>
+        
 
             <p>
                 Our mission is to get people who are ready to work in an environment where they have to tackle real work issues. With minimal guidance, instinct kicks and the best ones come out of the lot and start the second phase of the internship where the tasks get increasingly harder.
