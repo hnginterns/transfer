3339ef51efbc89168828bf049761f2b922a82c20
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-10 16:53:47 +0100

    Update features.blade.php

--- resources/views/pages/features.blade.php
+++ resources/views/pages/features.blade.php
@@ -4,27 +4,27 @@
 @section('content')
 
 <p><h3>FEATURES OF TRANSFER RULES</h3><p>
-<center>Our custom built Transfer Rules is built with the aim to connect your business to clients, employer to employee etc in a more efficient way and simplify your financial transactions.
+Our custom built Transfer Rules is built with the aim to connect your business to clients, employer to employee etc in a more efficient way and simplify your financial transactions.
 Crisp and clear UX designs and excellent workflow serves to foster customer intimacy and user loyalty.</center><p>
 
 <h3>WHAT FEATURE DO WE OFFER?</h3><br>
-1. Admin Interface <p>
-2. Credit Score <p>
-3. Personal Finance <p>
-4. Account tracking <p>
-5. User Interface <p>
-6. Multiple Wallet support <p>
-7. Wallet to Wallet Transactions <p>
-8. Wallet to Account Transactions <p>
-9. Account Balance check <p>
-10.Detailed Transaction History <p>
-11.Data Protection <p>
-12.Robust Financial Statistics and custom reports <p>
-13.Credit/Data Top-up <p>
-14.Bulk Sms Integration <P>
-15.Live Chat <p>
-16.Secured and Reliable Payment method <p>
-17.Easy usage with fewer clicks to make payments<p>
+1. Admin Interface <br>
+2. Credit Score <br>
+3. Personal Finance <br>
+4. Account tracking <br>
+5. User Interface <br>
+6. Multiple Wallet support <br>
+7. Wallet to Wallet Transaction <br>
+8. Wallet to Account Transactions <br>
+9. Account Balance check <br>
+10.Detailed Transaction History <br>
+11.Data Protection <p><br>
+12.Robust Financial Statistics and custom reports <br>
+13.Credit/Data Top-up <br>
+14.Bulk Sms Integration <br>
+15.Live Chat <p><br>
+16.Secured and Reliable Payment method <br>
+17.Easy usage with fewer clicks to make payments<br>
 <br>
  
 When it comes to financial app development as this, our priority lies on security, clean performance and long term viability
