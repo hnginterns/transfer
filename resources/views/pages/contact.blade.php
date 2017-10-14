f9879633fba0f7d1d28a26faebb54b9a17c6091c
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-14 10:35:31 +0100

    Update contact.blade.php

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.page')
+@extends('layouts.contact')
 @section('main-text', '')
 @section('sub-text', 'TransferRules lets you receive payments locally and globally with no hassles and zero set up fees')
 @section('content')
