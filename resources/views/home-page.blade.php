b445b333a6e19a2528bea79d31f80bc8972de771
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-14 18:48:24 +0100

    Update home-page.blade.php

--- resources/views/home-page.blade.php
+++ resources/views/home-page.blade.php
@@ -888,7 +888,7 @@
                 Create an account in less than 5 minutes. Login to your account and create wallets for your company
             </p>
         </div>
-        <button id="divider-cta">Sign Up Now</button>
+        <button id="divider-cta">Read More</button>
     </div>
 </div>
 <!-- DIVIDER ENDS-->
