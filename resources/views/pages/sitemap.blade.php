b3106bb8c68aea738b273a5ee3fbf5100f4a6e3f
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-14 17:42:48 +0100

    Create sitemap.blade.php

---
+++ resources/views/pages/sitemap.blade.php
@@ -0,0 +1,154 @@
+@extends('layouts.page')
+@section('main-text', 'Features')
+@section('sub-text', 'TransferRules lets you receive payments locally and globally with no hassles and zero set up fees')
+@section('content')
+
+<p><h3>SITE MAP</h3><p>
+<style type="text/css">
+body {
+background-color: #DDD;
+font: normal 80%  "Trebuchet MS", "Helvetica", sans-serif;
+margin:0;
+text-align:center;
+}
+#cont{
+margin:auto;
+width:800px;
+text-align:left;
+}
+a:link,a:visited {
+color: #0180AF;
+text-decoration: underline;
+}
+a:hover {
+color: #666;
+}
+h1 {
+background-color:#fff;
+padding:20px;
+color:#00AEEF;
+text-align:left;
+font-size:32px;
+margin:0px;
+}
+h3 {
+font-size:12px;
+background-color:#B8DCE9;
+margin:0px;
+padding:10px;
+}
+h3 a {
+float:right;
+font-weight:normal;
+display:block;
+}
+th {
+text-align:center;
+background-color:#00AEEF;
+color:#fff;
+padding:4px;
+font-weight:normal;
+font-size:12px;
+}
+td {
+font-size:12px;
+padding:3px;
+text-align:left;
+}
+tr {background: #fff}
+tr:nth-child(odd) {background: #f0f0f0}
+#footer {
+background-color:#B8DCE9;
+padding:10px;
+}
+.pager,.pager a {
+background-color:#00AEEF;
+color:#fff;
+padding:3px;
+}
+.lhead {
+background-color:#fff;
+padding:3px;
+font-weight:bold;
+font-size:16px;
+}
+.lpart {
+background-color:#f0f0f0;
+padding:0px;
+}
+.lpage {
+font:normal 12px verdana;
+}
+.lcount {
+background-color:#00AEEF;
+color:#fff;
+padding:2px;
+margin:2px;
+font:bold 12px verdana;
+}
+a.aemphasis {
+color:#009;
+font-weight:bold;
+}
+</style>
+
+</head>
+<body>
+<div id="cont">
+<h1>Finance.Hotels.Ng</h1>
+<h3><a href="https://finance.hotels.ng">Homepage</a>
+
+</h3>
+
+
+<table cellpadding="0" cellspacing="0" border="0" width="100%">
+
+<tr valign="top">
+<td class="lpart" colspan="100"><div class="lhead">/
+<span class="lcount">9 pages</span></div>
+
+<table cellpadding="0" cellspacing="0" border="0" width="100%">
+
+<tr><td class="lpage"><a href="https://finance.hotels.ng/" title="Transfer Rules">Transfer Rules</a></td></tr>
+<tr><td class="lpage"><a href="https://finance.hotels.ng/how-it-works" title="Transfer Rules">How it works</a></td></tr>
+<tr><td class="lpage"><a href="https://finance.hotels.ng/features" title="Transfer Rules">Features</a></td></tr>
+<tr><td class="lpage"><a href="https://finance.hotels.ng/about" title="Transfer Rules">About</a></td></tr>
+<tr><td class="lpage"><a href="https://finance.hotels.ng/privacy" title="Transfer Rules">Privacy</a></td></tr>
+<tr><td class="lpage"><a href="https://finance.hotels.ng/contact" title="Transfer Rules">Contact pge</a></td></tr>
+<tr><td class="lpage"><a href="https://finance.hotels.ng/help" title="Transfer Rules">Help</a></td></tr>
+<tr><td class="lpage"><a href="https://finance.hotels.ng/login" title="Sign In">Sign In</a></td></tr>
+<tr><td class="lpage"><a href="https://finance.hotels.ng/terms" title="Transfer Rules">Terms and Condition</a></td></tr>
+</table>
+</td>
+</tr>
+
+<tr valign="top">
+<td class="lbullet">&nbsp;&nbsp;&nbsp;&nbsp;</td>
+<td class="lpart" colspan="99"><div class="lhead">Admin/
+<span class="lcount">1 page</span></div>
+
+<table cellpadding="0" cellspacing="0" border="0" width="100%">
+
+<tr><td class="lpage"><a href="https://finance.hotels.ng/admin/login" title="Sign In">Sign In</a></td></tr>
+</table>
+</td>
+</tr>
+
+<tr valign="top">
+<td class="lbullet">&nbsp;&nbsp;&nbsp;&nbsp;</td>
+<td class="lpart" colspan="99"><div class="lhead">password/
+<span class="lcount">1 page</span></div>
+
+<table cellpadding="0" cellspacing="0" border="0" width="100%">
+
+<tr><td class="lpage"><a href="https://finance.hotels.ng/password/reset" title="https://finance.hotels.ng/password/reset">https://finance.hotels.ng/password/reset</a></td></tr>
+</table>
+</td>
+</tr>
+
+</table>
+
+            </p>
+
+
+@endsection
