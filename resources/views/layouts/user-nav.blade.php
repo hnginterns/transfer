2df84c7e60a1a289063a464731fe6266fb4fa84f
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-16 00:12:25 +0100

    Update user-nav.blade.php

--- resources/views/layouts/user-nav.blade.php
+++ resources/views/layouts/user-nav.blade.php
@@ -10,53 +10,8 @@
           </button>
           
         </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li class="hidden-lg hidden-lg-up">
-                <a href="/dashboard">
-                <i class="fa fa-dashboard fa-lg"></i> Dashboard
-                </a>
-              </li>  
-                <li  class="hidden-lg hidden-lg-up">
-                <a href="/wallet-view">
-                <i class="fa fa-tasks fa-lg"></i> Wallet View
-                </a>
-              </li>  
-                <li  class="hidden-lg hidden-lg-up">
-                <a href="/transfer-to-wallet">
-                <i class="fa fa-tasks fa-lg"></i> Wallet Transfer
-                </a>
-              </li>  
-                <li  class="hidden-lg hidden-lg-up">
-                <a href="transfer-to-bank">
-                <i class="fa fa-bank fa-lg"></i> Bank Transfer
-                </a>
-              </li>  
-                <li  class="hidden-lg hidden-lg-up">
-                <a href="{{ url('/logout') }}">
-                <i class="fa fa-dashboard fa-lg"></i> Logout
-                </a>
-              </li>
-              @if(Auth::user())
-              </li>  
-                <li  class="hidden-lg hidden-lg-up">
-                <a href="{{ url('/dashboard') }}">
-                <i class="fa fa-dashboard fa-lg"></i> Dashboard
-                </a>
-              </li>
-              @endif
-
-              @if(Auth::user()->isAdmin())
-              </li>  
-                <li  class="hidden-lg hidden-lg-up">
-                <a href="{{ url('/admin') }}">
-                <i class="fa fa-dashboard fa-lg"></i> Dashboard
-                </a>
-              </li>
-              @endif
-              <li><a href="#" style="color:#39689C; font-size:18px;"><i class="fa fa-user"></i>  </a></li>
-          </ul>
-          <form class="navbar-form navbar-right">
+         <li><a href="#" style="color:#39689C; font-size:18px;"><i class="fa fa-user"></i>  </a></li>
+           <form class="navbar-form navbar-right">
             <input type="text" class="form-control" placeholder="Search...">
             <!-- Right Section -->
                     <div class="content-header-section">
