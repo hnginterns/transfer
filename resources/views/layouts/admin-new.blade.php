08670974b1ff82e2d41ac84a36363abf288d57a8
Emmanuel Joseph <jekayode@live.com>
2017-10-15 01:53:18 +0100

    Admin new layout test

--- resources/views/layouts/admin-new.blade.php
+++ resources/views/layouts/admin-new.blade.php
@@ -102,126 +102,64 @@
 
   <!-- Middle Section -->
                     <div class="content-header-section d-none d-lg-block">
-                        <ul class="nav-main">
-                                <li>
-                                    <a class="active" href="bd_dashboard.html"><i class="si si-compass"></i>Dashboard</a>
-                                </li>
-                                <li>
-                                    <a href="be_pages_dashboard.html"><i class="si si-action-undo"></i>Wallets</a>
-                                </li>
+                        <!-- Header Navigation -->
+                        <!--
+                        Desktop Navigation, mobile navigation can be found in #sidebar
+
+                        If you would like to use the same navigation in both mobiles and desktops, you can use exactly the same markup inside sidebar and header navigation ul lists
+                        If your sidebar menu includes headings, they won't be visible in your header navigation by default
+                        If your sidebar menu includes icons and you would like to hide them, you can add the class 'nav-main-header-no-icons'
+                        -->
+                        <ul class="nav-main-header">
+                            <li>
+                                <a class="active" href="bd_dashboard.html"><i class="si si-compass"></i>Dashboard</a>
+                            </li>
+                          
+                            <li>
+                                <a href="#"><i class="si si-action-undo"></i>Wallets</a>
 
                                 <li>
-                                    <a href="be_pages_dashboard.html"><i class="si si-action-undo"></i>Users</a>
-                                </li>
+                                <a href="#"><i class="si si-action-undo"></i>Users</a>
+                            </li>
 
-                                <li>
-                                    <a href="be_pages_dashboard.html"><i class="si si-action-undo"></i>Permissions</a>
-                                </li>
+                            <li>
+                                <a href="#"><i class="si si-action-undo"></i>Permissions</a>
+                            </li>
 
-                                <li>
-                                    <a href="be_pages_dashboard.html"><i class="si si-action-undo"></i>SMS Wallet</a>
-                                </li>
-
-
-                            </ul>
+                            <li>
+                                <a href="#"><i class="si si-action-undo"></i>SMS Wallet</a>
+                            </li>
+                            </li>
+                        </ul>
+                        <!-- END Header Navigation -->
                     </div>
                     <!-- END Middle Section -->
 
                     <!-- Right Section -->
                     <div class="content-header-section">
-                        <!-- Color Themes + A few of the many header options (used just for demonstration) -->
-                        <!-- Themes functionality initialized in Codebase() -> uiHandleTheme() -->
-                        <div class="btn-group ml-5" role="group">
-                            <button type="button" class="btn btn-circle btn-dual-secondary" id="page-header-themes-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fa fa-paint-brush"></i>
+
+                      <!-- User Dropdown -->
+                        <div class="btn-group" role="group">
+                            <button type="button" class="btn btn-rounded btn-dual-secondary" id="page-header-user-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                J. Smith<i class="fa fa-angle-down ml-5"></i>
                             </button>
-                            <div class="dropdown-menu dropdown-menu-right min-width-150" aria-labelledby="page-header-themes-dropdown">
-                                <h6 class="dropdown-header text-center">Color Themes</h6>
-                                <div class="row no-gutters text-center">
-                                    <div class="col-4 mb-5">
-                                        <a class="text-default" data-toggle="theme" data-theme="default" href="javascript:void(0)">
-                                            <i class="fa fa-2x fa-circle"></i>
-                                        </a>
-                                    </div>
-                                    <div class="col-4 mb-5">
-                                        <a class="text-elegance" data-toggle="theme" data-theme="assets/css/themes/elegance.min.css" href="javascript:void(0)">
-                                            <i class="fa fa-2x fa-circle"></i>
-                                        </a>
-                                    </div>
-                                    <div class="col-4 mb-5">
-                                        <a class="text-pulse" data-toggle="theme" data-theme="assets/css/themes/pulse.min.css" href="javascript:void(0)">
-                                            <i class="fa fa-2x fa-circle"></i>
-                                        </a>
-                                    </div>
-                                    <div class="col-4 mb-5">
-                                        <a class="text-flat" data-toggle="theme" data-theme="assets/css/themes/flat.min.css" href="javascript:void(0)">
-                                            <i class="fa fa-2x fa-circle"></i>
-                                        </a>
-                                    </div>
-                                    <div class="col-4 mb-5">
-                                        <a class="text-corporate" data-toggle="theme" data-theme="assets/css/themes/corporate.min.css" href="javascript:void(0)">
-                                            <i class="fa fa-2x fa-circle"></i>
-                                        </a>
-                                    </div>
-                                    <div class="col-4 mb-5">
-                                        <a class="text-earth" data-toggle="theme" data-theme="assets/css/themes/earth.min.css" href="javascript:void(0)">
-                                            <i class="fa fa-2x fa-circle"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                                <h6 class="dropdown-header text-center">Header</h6>
-                                <button type="button" class="btn btn-sm btn-block btn-alt-secondary" data-toggle="layout" data-action="header_fixed_toggle">Fixed Mode</button>
-                                <button type="button" class="btn btn-sm btn-block btn-alt-secondary mb-10" data-toggle="layout" data-action="header_style_inverse_toggle">Style</button>
+                            <div class="dropdown-menu dropdown-menu-right min-width-150" aria-labelledby="page-header-user-dropdown">
+                                <a class="dropdown-item" href="be_pages_generic_profile.html">
+                                    <i class="si si-user mr-5"></i> Profile
+                                </a>
+                               
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="be_layout_api.html">
-                                    <i class="si si-chemistry"></i> All Options (API)
+                                <a class="dropdown-item" href="op_auth_signin.html">
+                                    <i class="si si-logout mr-5"></i> Logout
                                 </a>
                             </div>
                         </div>
-                        <!-- END Color Themes + A few of the many header options -->
-
-                        <!-- Open Search Section -->
-                        <!-- Layout API, functionality initialized in Codebase() -> uiApiLayout() -->
-                        <button type="button" class="btn btn-circle btn-dual-secondary" data-toggle="layout" data-action="header_search_on">
-                            <i class="fa fa-search"></i>
-                        </button>
-                        <!-- END Open Search Section -->
-
-                        <!-- Toggle Sidebar -->
-                        <!-- Layout API, functionality initialized in Codebase() -> uiApiLayout() -->
-                        <button type="button" class="btn btn-circle btn-dual-secondary d-lg-none" data-toggle="layout" data-action="sidebar_toggle">
-                            <i class="fa fa-navicon"></i>
-                        </button>
-                        <!-- END Toggle Sidebar -->
+                        <!-- END User Dropdown -->
+                        
                     </div>
                     <!-- END Right Section -->
                 </div>
                 <!-- END Header Content -->
-
-                <!-- Header Search -->
-                <div id="page-header-search" class="overlay-header">
-                    <div class="content-header content-header-fullrow">
-                        <form action="bd_search.html" method="post">
-                            <div class="input-group">
-                                <span class="input-group-btn">
-                                    <!-- Close Search Section -->
-                                    <!-- Layout API, functionality initialized in Codebase() -> uiApiLayout() -->
-                                    <button type="button" class="btn btn-secondary px-15" data-toggle="layout" data-action="header_search_off">
-                                        <i class="fa fa-times"></i>
-                                    </button>
-                                    <!-- END Close Search Section -->
-                                </span>
-                                <input type="text" class="form-control" placeholder="Search or hit ESC.." id="page-header-search-input" name="page-header-search-input">
-                                <span class="input-group-btn">
-                                    <button type="submit" class="btn btn-secondary px-15">
-                                        <i class="fa fa-search"></i>
-                                    </button>
-                                </span>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <!-- END Header Search -->
 
                 <!-- Header Loader -->
                 <!-- Please check out the Activity page under Elements category to see examples of showing/hiding it -->
