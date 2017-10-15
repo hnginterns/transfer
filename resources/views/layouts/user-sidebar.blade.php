1434d56a5727ac69202f89384eb99bc3813b6823
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-15 23:00:02 +0100

    Update user-sidebar.blade.php

--- resources/views/layouts/user-sidebar.blade.php
+++ resources/views/layouts/user-sidebar.blade.php
@@ -8,20 +8,20 @@
                 <a href="/dashboard">
                 <i class="fa fa-dashboard fa-lg"></i> Dashboard
                 </a>
-              </li> 
+              </li> <br><br>
 
 
               <li>
                 <a href="/phonetopup">
                 <i class="fa fa-phone fa-lg"></i> Phone TopUp
                 </a>
-              </li> 
+              </li> <br><br>
               
               <li>
                 <a href="/admin/smswallet">
                 <i class="fa fa-envelope"></i> Sms Top up
                 </a>
-              </li> 
+              </li> <br><br>
                                 
               <li>
                 <a href="{{ url('/logout') }}">
