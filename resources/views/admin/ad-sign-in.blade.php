4c25e49c45f6ab46d00c9f9e08950221fa6b655a
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-16 20:54:49 +0100

    Update ad-sign-in.blade.php

--- resources/views/admin/ad-sign-in.blade.php
+++ resources/views/admin/ad-sign-in.blade.php
@@ -57,7 +57,7 @@
 
                     <li class="nav-item">
 
-                        <a class="nav-link" href="/home">Home
+                        <a class="nav-link" href="/">Home
 
                             <span class="sr-only">(current)</span>
 
