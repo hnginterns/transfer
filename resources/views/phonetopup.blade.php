70ba1a0d12fddf3014cec925e6714cd4c2f4896c
unknown <tobiagbola@gmail.com>
2017-10-16 07:59:03 +0100

    phone UI

--- resources/views/phonetopup.blade.php
+++ resources/views/phonetopup.blade.php
@@ -2,8 +2,8 @@
 @section('title', 'Dashboard')
 @section('content')
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.1.css">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="/css/style.1.css">
+    <link rel="stylesheet" href="/css/main.css">
 <div class="row">
         <div class="col-lg-12">
         <div class="content_two">
