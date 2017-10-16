3b882d42abd80b65401c575d54263f1abf4b39b7
Franklin Nwanze <nwanzechuka1993@gmail.com>
2017-10-16 10:18:59 +0100

    removed non existing route link on home page

--- app/Notifications/PasswordReset.php
+++ app/Notifications/PasswordReset.php
@@ -7,7 +7,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class PasswordReset extends Notification
+class PasswordReset extends Notification implements ShouldQueue
 {
     use Queueable;
 
