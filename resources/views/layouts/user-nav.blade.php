d7a95ba863d3171c994789143e7eee68048bee52
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-16 02:24:40 +0100

    Update user-nav.blade.php

--- resources/views/layouts/user-nav.blade.php
+++ resources/views/layouts/user-nav.blade.php
@@ -1,6 +1,6 @@
       <nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
-      <a class="navbar-brand" href="{{url('/')}}">  <img src="/img/logo3.png" alt=""><span></span></a>
+      <a href="{{url('/')}}">  <img src="/img/logo3.png" alt=""></a>
         <div class="navbar-header">
         
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
