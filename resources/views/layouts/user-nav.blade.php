d9525aeb1e654c4620a6ab7746fbaebe6a0161a0
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-16 03:16:56 +0100

    Update user-nav.blade.php

--- resources/views/layouts/user-nav.blade.php
+++ resources/views/layouts/user-nav.blade.php
@@ -10,20 +10,11 @@
           </div>
           <form class="navbar-form navbar-right">
             <input type="text" class="form-control" placeholder="Search...">
-            
-        <div class="btn-group open">
-            <a class="btn btn-primary" href="#"><i class="fa fa-user fa-fw"></i> User</a>
-            <a class="btn btn-primary dropdown-toggle" data-toggle="dropdown" href="#">
-            <span class="fa fa-caret-down" title="Toggle dropdown menu"></span></a>
-            <ul class="dropdown-menu">
-                  <li><a href="#"><i class="fa fa-pencil fa-fw"></i> Edit</a></li>
-                  <li><a href="#"><i class="fa fa-trash-o fa-fw"></i> Delete</a></li>
-                  <li><a href="#"><i class="fa fa-ban fa-fw"></i> Ban</a></li>
-                  <li class="divider"></li>
-                  <li><a href="#"><i class="fa fa-unlock"></i> Make admin</a></li>
+       
                   </ul>
              </div>
             Godfred Akpan | Admin &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <i class="fa fa-user-circle-o"></i>
            </form>
         </div>
       </div>
