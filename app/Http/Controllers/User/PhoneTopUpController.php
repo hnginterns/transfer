9725cab6a4642a53579dffd15704083fb84183bf
AniekanE <hayley.essien94@gmail.com>
2017-10-16 16:32:14 +0100

    Update PhoneTopUpController.php

--- app/Http/Controllers/User/PhoneTopUpController.php
+++ app/Http/Controllers/User/PhoneTopUpController.php
@@ -1,1 +1,37 @@
-
+<?php
+namespace App\Http\Controllers\User;
+use DateTime;
+use App\Wallet;
+use App\WalletTransaction;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Restriction;
+use App\CardWallet;
+use App\Beneficiary;
+use App\Rule;
+use App\Transaction;
+use URL;
+use App\BankTransaction;
+use RestrictionController;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\RestrictionController as Restrict;
+use App\Events\TransferToBank;
+use App\Events\FundWallet;
+class PhoneTopUpController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+   public function phoneTopUp()
+    {
+        //
+    }
+}
