e96d566a8d82ca237c11ebd2e780137a2bf95489
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-16 23:02:18 +0100

    Update view-wallet.blade.php

--- resources/views/view-wallet.blade.php
+++ resources/views/view-wallet.blade.php
@@ -33,6 +33,23 @@ i.sent{
 
     i.received{
       color: #dd4b39;
+    
+    table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: center;
+    padding: 5px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+    
     }
 </style>
 
