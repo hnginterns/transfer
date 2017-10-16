c705c1a9db1988fdf4c53c07483132ce946c2202
Agbola Tobi <tobiagbola@gmail.com>
2017-10-16 06:52:04 +0100

    phone top up UI fix

--- resources/views/phonetopup.blade.php
+++ resources/views/phonetopup.blade.php
@@ -378,8 +378,6 @@
            </div>
            
             </div>
-
-@endsection      
 <script
         src="https://code.jquery.com/jquery-3.2.1.min.js"
         integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
@@ -387,3 +385,4 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script type="text/javascript" src="http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/flwv3-pug/getpaidx/api/flwpbf-inline.js"></script>
     <script type="text/javascript" src="js/wallet.js"></script>
+@endsection      
