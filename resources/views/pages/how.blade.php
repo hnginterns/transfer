64179d0a438425639376574b49765c8d8815424a
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-17 15:36:32 +0100

    Update how.blade.php

--- resources/views/pages/how.blade.php
+++ resources/views/pages/how.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.page')
+@extends('layouts.fag')
 @section('main-text', 'FREQUENTLY ASKED QUESTION (FAQs)')
 @section('sub-text', 'Finance@ Hotels.ng lets an admin manage bills and fund disbursement')
 @section('content')
