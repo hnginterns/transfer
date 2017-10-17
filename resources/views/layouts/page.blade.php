ada6fea75791c6e59fa3ca39230cfd96b0457fc1
mercyikpe <ikpemercy1@gmail.com>
2017-10-17 11:24:24 +0100

    Update page.blade.php

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -11,20 +11,140 @@
 
     <title>Finance@hotels.ng</title>
 
-    <link href="{{asset('css/bootstraps.css')}}" rel="stylesheet">
+    <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet">
 
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet">
 
     <link href="{{asset('css/signin.css')}}" rel="stylesheet">
 
-    <link href="{{asset('css/faqs.css')}}"
+    <link href="{{asset('css/custom2.css')}}"
 
     </head>
 
 
 <body>
+<nav class="navbar navbar-expand-md navbar-light fixed-top">
+        <a class="navbar-brand" href="#"><img src="img/logo.png" alt="logo"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
+        <div class="collapse navbar-collapse pull-right" id="navbarsExampleDefault">
+            <ul class="navbar-nav ml-auto ">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="projects.html">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="participants.html">FAQs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="blog.html">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link btn nav-btn" href="blogpage.html">Sign In</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     
+    
+    <!--      footer -->
+    <section class="container-fuild content">
+
+    </section>
+    <hr>
+
+    <footer id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 mx-auto">
+                    <div class="footer-link-header">
+                        <h3>Company</h3>
+                    </div>
+                    <ul class="footer-nav">
+                        <li><a href="">Blog</a></li>
+                        <li><a href="">About Us</a></li>
+                        <li><a href="">Press Page</a></li>
+                        <li><a href="">Job Opening</a></li>
+                        <li><a href="">Trade &amp; Advertising</a></li>
+                        <li><a href="">Terms &amp; Condition</a></li>
+                        <li><a href="">Privacy &amp; policy</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 mx-auto">
+                    <div class="footer-link-header">
+                        <h3>Community</h3>
+                    </div>
+                    <ul class="footer-nav">
+                        <li><a href="">Twitter</a></li>
+                        <li><a href="">Instagram</a></li>
+                        <li><a href="">Google</a></li>
+                        <li><a href="">Pintrest</a></li>
+                        <li><a href="">Youtube</a></li>
+                        <li><a href="">Traveller's Blog</a></li>
+                        <li><a href="">Tourist Attractions</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 ">
+                    <div class="footer-link-header">
+                        <h3>Flights</h3>
+                    </div>
+                    <ul class="footer-nav">
+                        <li><a href="">Abuja to Lagos</a></li>
+                        <li><a href="">Fantastic Fligt Deals</a></li>
+                        <li><a href="">Cheap Flights to Lagos</a></li>
+                        <li><a href="">Chartered Flights</a></li>
+                        <li><a href="">Calabar to Lagos</a></li>
+                        <li><a href="">Flight to Lagos</a></li>
+                        <li><a href="">Best Prices</a></li>
+                        <li><a href="">Uyo to Lagos</a></li>
+                </div>
+
+            </div>
+        </div>
+        <hr>
+        <div class="bottom-footer">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>Copyright &copy; 2017 Hotels.ng All Rights Reserved.</p>
+                </div>
+                <div class="col-md-4  certify">
+                    <p>Certified by </p>
+                    <div class="cert-imgs">
+                        <img src="img/fi1.png">
+                        <img src="img/fi2.png">
+                        <img src="img/fi3.png">
+                    </div>
+                </div>
+            </div>
+            <div class="chat-box">
+                <p>Let's Chat! <img src="img/chat.png" class="pull-right"></p>
+
+            </div>
+
+        </div>
+    </footer>
+    </div>
+    <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>-->
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+    <!--Customize javascript -->
+
+
+    <script type="text/javascript" src="js/phonetopup.js"></script>
+
 </body>
 </html>
 
