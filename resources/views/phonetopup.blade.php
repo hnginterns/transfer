67e018208c221b60d8677db127be42affdfcc352
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-18 02:32:34 +0100

    Update phonetopup.blade.php

--- resources/views/phonetopup.blade.php
+++ resources/views/phonetopup.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.user')
 
 @section('title')
-      Transfer to Bank
+      Phone Top up
 @endsection
 @section('content')
 <style>
@@ -229,34 +229,12 @@ tr:nth-child(even) {
             </table>    <br><br>
 		</div>
 
-     @if (session('status'))
-   <script type="text/javascript">
-        $(document).ready(function() {
-            $('#otpModal').modal();
-        });
-    </script>
+ 
 
     <div class="modal fade" id="otpModal" role="dialog">
     <div class="modal-dialog">
     
-      <!-- Modal content-->
-      <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title">Otp</h4>
-          </div>
-          <div class="modal-body">
-            <p>{{session('status')}}</p>
-            <div class="row">
-            <div class="col-md-6 col-md-offset-2">
-              <form action="/otp" method="POST">
-                {{csrf_field()}}
-                <input type="hidden" name="ref" value="{{$cardWallet->ref}}">
-                <div class="form-group">
-                    <input type="password" class="form-control" name="otp" placeholder="Enter OTP">
-                </div>
-                <button type="submit" class="btn btn-default btn-block">Submit</button>
-              </form>
+      
             </div>
           </div>
       </div>
