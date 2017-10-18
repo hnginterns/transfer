9f232efc06e011b1b003f3210ca4e9cea08016c5
Joseph Bassey <joseph.mbassey2@gmail.com>
2017-10-18 04:15:44 +0100

    Update topuptest.blade.php

--- resources/views/topuptest.blade.php
+++ resources/views/topuptest.blade.php
@@ -701,7 +701,92 @@
 
    
 
+ <div class="container">
+                    <hr>
 
+                    <p>
+                        <h3>Transaction History </h3>
+                    </p>
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <td>phone number</td>
+                                <td>Amount</td>
+                                <td>Network</td>
+                                <td>type</td>
+                                <td>status</td>
+                                <td>Date</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>07061926206</td>
+                                <td>2000</td>
+                                <td>MTN</td>
+                                <td>Airtime</td>
+                                <td>Succes</td>
+                                <td>12.09.13</td>
+
+                            </tr>
+                            <tr>
+                                <td>07061926206</td>
+                                <td>2000</td>
+                                <td>MTN</td>
+                                <td>Airtime</td>
+                                <td>Succes</td>
+                                <td>12.09.13</td>
+
+                            </tr>
+                            <tr>
+                                <td>07061926206</td>
+                                <td>2000</td>
+                                <td>MTN</td>
+                                <td>Airtime</td>
+                                <td>Succes</td>
+                                <td>12.09.13</td>
+
+
+                            </tr>
+                            <tr>
+                                <td>07061926206</td>
+                                <td>2000</td>
+                                <td>MTN</td>
+                                <td>Airtime</td>
+                                <td>Succes</td>
+                                <td>12.09.13</td>
+
+
+                            </tr>
+                            <tr>
+                                <td>07061926206</td>
+                                <td>2000</td>
+                                <td>MTN</td>
+                                <td>Airtime</td>
+                                <td>Succes</td>
+                                <td>12.09.13</td>
+
+                            </tr>
+                            <tr>
+                                <td>07061926206</td>
+                                <td>2000</td>
+                                <td>MTN</td>
+                                <td>Airtime</td>
+                                <td>Succes</td>
+                                <td>12.09.13</td>
+
+
+                            </tr>
+
+
+                            </tr>
+                        </tbody>
+                    </table>
+                    <hr>
+                </div>
+
+
+                </div>
 
 
 
