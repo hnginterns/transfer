a76e3280b5fea9346003b5e0f70b55f7663feeaf
Emmanuel Joseph <jekayode@live.com>
2017-10-18 16:27:30 +0100

    Get Topup Wallet balance

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -34,26 +34,28 @@ class AdminController extends WalletController
         $username = '08189115870';
         $pass =  'dbcc49ee2fba9f150c5e82';
 
-        $url = "https://mobileairtimeng.com/httpapi/balance.php?userid=" 
-            . urlencode($username). "&pass=". urlencode($pass);
-
-        $curlConnection = curl_init();
-        curl_setopt($curlConnection, CURLOPT_HTTPHEADER, array("cache-control: no-cache",
-              "postman-token: 28c061c4-a48c-629f-3aa2-3e4cad0641ff"));
-        curl_setopt($curlConnection, CURLOPT_URL, $url);
-        curl_setopt($curlConnection, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($curlConnection, CURLOPT_FOLLOWLOCATION, TRUE);
-        curl_setopt($curlConnection, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curlConnection, CURLOPT_SSL_VERIFYPEER, FALSE);
-
-        $results = json_decode(curl_exec($curlConnection), true);
-
-        if (isset($results['error'])) {
-            var_dump($results);
-            exit();
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => "https://mobileairtimeng.com/httpapi/?userid=%2008189115870&pass=dbcc49ee2fba9f150c5e82&network=1&phone=08026737118&amt=$amount",
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 30,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "GET",
+          CURLOPT_HTTPHEADER => array(
+            "cache-control: no-cache",
+            "postman-token: 1d2f6463-e42b-b5bd-00ac-83d72d17d377"
+          ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        if ($err) {
+          return "cURL Error #:" . $err;
+        } else {
+          return $response;
         }
-
-        return $results;
     }
 
     public function phoneTopupView()
