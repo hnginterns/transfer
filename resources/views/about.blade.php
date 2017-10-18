4ceedabac8ce0b2681952de57d67fa4f6e483236
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-18 11:19:45 +0100

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -46,7 +46,7 @@
         <p id="sub-heading">
             It’s simple, no stress, no fuss.
         </p>
-        <button id="get-started">GET STARTED</button>
+        <button id="get-started"><a href="{{url('/')}}">GET STARTED</button></a>
     </div>
     <img src="img/get-started.png" alt="get started image">
 </div>
