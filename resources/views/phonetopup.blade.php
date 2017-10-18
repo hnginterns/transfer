f5dc470cada789277a08c4eb6ef0a6abc24b8863
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-18 08:34:15 +0100

    Update phonetopup.blade.php

--- resources/views/phonetopup.blade.php
+++ resources/views/phonetopup.blade.php
@@ -146,7 +146,8 @@ tr:nth-child(even) {
                             <option value="50000">50000</option>
                             <option value="100000">100000</option>
                             
-   </select>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button id="" type="submit" class="btn btn-primary pull-right">Tup Up</button></td>
+   </select>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   <button id="" type="submit" class="btn btn-dark">Tup Up</button></td>
     <td height="200">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -164,7 +165,7 @@ tr:nth-child(even) {
                             <option value="50000">50000</option>
                             <option value="100000">100000</option>
                             
-   </select>&nbsp;&nbsp;&nbsp;&nbsp;<button id="btn btn-primary btn-block" type="submit"   class="btn btn-primary pull-right">Top Up</button></td>
+   </select>&nbsp;&nbsp;&nbsp;&nbsp;<button id="btn btn-primary btn-block" type="submit"   class="btn btn-dark">Top Up</button></td>
     
     </tr>
                
