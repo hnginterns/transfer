f5b6dbf2d9a3df84cc048cd775b298ac71dbf32c
Emmanuel Joseph <jekayode@live.com>
2017-10-20 17:14:59 +0100

    Topup Wallet Funding History

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -79,7 +79,10 @@ class AdminController extends WalletController
 
          $wallet = Wallet::where('type', 'topup')->first();
          //DB::table('wallets')->where('type', '=','topup')->first();
-         $cardWallet = CardWallet::where('type', 'topup')->get();
+         
+         $wallet_name = $wallet->wallet_name;
+         $cardWallet = CardWallet::where('wallet_name', $wallet_name)->get();
+
          dd($cardWallet);
         
         return view('admin.phonetopup.index', compact('phones', 'wallet', 'bank', 'topupbalance', 'contacts', 'cardwallets'));
