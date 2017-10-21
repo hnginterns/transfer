73b7377b0d52cdac42b12945d51cadebc5d6f584
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-21 21:50:33 +0100

    Update smswallet2.blade.php

--- resources/views/admin/smswallet2.blade.php
+++ resources/views/admin/smswallet2.blade.php
@@ -9,12 +9,32 @@
         /* Anytime the body has the loading class, our
         modal element will be visible */
         body.loading .modal-loading{display: block;}
+	.units {
+          background:#222d32;
+          color: #fff;
+          margin:1%;
+          padding:2%;}
+	 #balRefre img {width:100% }
+     #walletTopUp .modal-header {
+       background-color: #222d32;
+       color: #fff;
+     }
+     #walletTopUp input, #walletTopUp select{
+      border: 1px solid #cccccc;
+      border-radius: 5px;
+      height: 50px;
+     }
+     #walletTopUp #amount {
+       border-radius: 0px;
+     }
+     #walletTopUp input:focus, #walletTopUp select:focus {
+       background-color: #cccccc;
+     } 
     </style>
 @endsection
 
 @section('content')
-
-    <section class="col-md-12">
+   <section class="col-md-12">
       <!-- Content Wrapper. Contains page content -->
       <div class="row">
         <div class="col-lg-6">
@@ -22,7 +42,7 @@
             SMS Wallet
             
           </h1>
-          <h2>Id:X846945532</h2>
+        
           <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
             <li class="active">Sms wallet</li>
@@ -31,35 +51,203 @@
         <div class="col-lg-3 col-lg-offset-3">
         <h2 id="balance">Balance: <span id="wallet-balance"></span></h2>
         <div>
-        <button type="button" class="btn btn-primary" id="topup"><!--data-toggle="modal" data-target="#walletTopUp">-->
-          Top up
+	<div class="icon"id="balRefre" style="cursor:pointer">
+				<i class="fa fa-refresh fa-2x" aria-hidden="true"></i>			
+			</div>
+        <div class="btn-group">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#walletTopUp">
+          Fund Wallet
         </button>
+	</div>
         </div>
         </div>
       </div><!--row ends-->
+      <!---Modal for wallet top Up-->
+    <div class="modal fade" id="walletTopUp">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title text-center">Wallet Top Up</h4>
+              </div>
+              <div class="modal-body">
+              <div class="box-header with-border">
+                <h3 class="box-title">Card Details</h3>
+              </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <form action="sms" method="POST" role="form form-horizontal">
+                {{csrf_field()}}
+                <!-- text input -->
+               <div class="container-fluid">
+                <fieldset>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <label for="cc_name">First Name</label>
+                                <div class="controls">
+                                    <input name="fname" class="form-control" id="cc_name" title="First Name" required type="text">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                          <div class="form-group">
+                              <label for="cc_name">Last Name</label>
+                              <div class="controls">
+                                  <input name="lname" class="form-control" id="cc_name"  title="last name" required type="text">
+                              </div>
+                          </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>Phone Number</label>
+                        <div class="controls">
+                              <input name="phone" class="form-control" autocomplete="off" maxlength="20"  required="" type="text">
+
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>Email Address</label>
+                        <div class="controls">
+                              <input name="emailaddr" class="form-control" autocomplete="off" required="" type="text">
+
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>Card Number</label>
+                        <div class="controls">
+                              <input name="card_no" class="form-control" autocomplete="off" maxlength="20"  required="" type="text">
+
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>Card Expiry Date</label>
+                        <div class="controls">
+                            <div class="row">
+                                <div class="col-md-9">
+                                    <select class="form-control" name="expiry_month">
+                                        <option value="01">January</option>
+                                        <option value="02">February</option>
+                                        <option value="03">March</option>
+                                        <option value="04">April</option>
+                                        <option value="05">May</option>
+                                        <option value="06">June</option>
+                                        <option value="07">July</option>
+                                        <option value="08">August</option>
+                                        <option value="09">September</option>
+                                        <option value="10">October</option>
+                                        <option value="11">November</option>
+                                        <option value="12">December</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-3">
+                                    <select class="form-control" name="expiry_year">
+				    @for ($i = 2017;$i <2040;$i++)
+                                        <option>{{$i}}</option>
+                                     @endfor  
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                      <div class="row">
+                        <div class="col-md-3">
+                               <label>Card CVV</label>
+                                <div class="controls">
+                                    <input class="form-control" autocomplete="off" maxlength="3" pattern="\d{3}" title="Three digits at back of your card" required="" type="text" name="cvv">
+                                </div>
+                         </div>
+
+                         <div class="col-md-3">
+                               <label>Pin</label>
+                                <div class="controls">
+                                    <input class="form-control" autocomplete="off" maxlength="4" pattern="\d{4}" title="pin" required="" type="text" name="pin">
+                                </div>
+                         </div>
+			
+                          <div class="col-md-6">
+                                  <label>Amount</label>
+                                  <div class="input-group">
+                                      <div class="input-group-addon">₦</div>
+                                      <input name="amount" type="text" class="form-control" id="amount" placeholder="Amount">
+                                    </div>
+                          </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label></label>
+                        <div class="controls">
+                            <button type="submit" class="btn btn-primary">Top up</button>
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                        </div>
+                    </div>
+                </fieldset>
+            </div>
+              </form>
+              </div>
+              
+            </div>
+	       <!-- /.modal-OTP -->
+
+            <!-- /.modal-content -->
+          </div>
+          <!-- /.modal-dialog -->
+        </div>
+        <!-- /.modal -->
     </section>
 
+
+      @if (session('status'))
+   <script type="text/javascript">
+        $(document).ready(function() {
+            $('#myModal').modal();
+        });
+    </script>
+
+    <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Otp</h4>
+          </div>
+          <div class="modal-body">
+            <p>{{session('status')}}</p>
+            <div class="row">
+            <div class="col-md-6 col-md-offset-2">
+              <form action="Otp" method="POST">
+                {{csrf_field()}}
+                <input type="hidden" name="ref" value="{{$sms->ref}}">
+                <div class="form-group">
+                    <input type="password" class="form-control" name="otp" placeholder="Enter OTP">
+                </div>
+                <button type="submit" class="btn btn-default btn-block">Submit</button>
+              </form>
+            </div>
+          </div>
+      </div>
+      
+    </div>
+  </div>
+
+</div>
+@endif
+    
     <!-- Main content -->
     <section class="content container" id="bulksms">
-
-        <button class="btn btn-success"><href="createsmswallet.blade.php"</a>Create New</button>
-        <hr />
-        <div class="row">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-<body>
-
-<div class="container">
-  <h3>Add SMS account</h3>
+ 
   <!-- Trigger the modal with a button -->
   <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">ADD SMS ACCOUNT</button>
-
-  <!--Add sms Account  Modal -->
+<!--modal for add sms account -->
+        <div class="container">
+    <!--Add sms Account  Modal -->
   <div class="modal fade" id="myModal" role="dialog">
     <div class="modal-dialog">
+    
     
       <!-- Modal content-->
       <div class="modal-content">
@@ -69,25 +257,63 @@
         </div>
         <div class="modal-body">
           <p>Please fill out your details</p>
-          <form role="form" class="submit-topup">
+          <form role="form" class="submit-add_sms_account">
                 {{ csrf_field() }}
                 <!-- text input -->
-				<div class="form-group">
-                  <label>Name</label>
-                  <input class="form-control email" name="email" type="email">
+		<div class="form-group">
+                  <div class="form-group">
+                  <label>Username</label>
+                  <input class="form-control email" name="username" type="email">
                 </div>
                 <div class="form-group">
-                  <label>Email</label>
-                  <input class="form-control email" name="email" type="email">
+                  <label>API Key</label>
+                  <input class="form-control email" name="api_id" type="text">
                 </div>
-                <div class="form-group">
-                  <label>Password</label>
-                  <input class="form-control app_id" name="password" type="text">
+		<div class="form-group">
+                  <label>Account Number</label>
+                  <input class="form-control email" name="bank_account" type="text">
                 </div>
-                 <div class="form-group">
-                  <label>Account Amount</label>
-                  <input class="form-control amount" type="text" name="amount">
+              		 <div class="form-group">
+				  <label> Bank Name</label>
+				    <select name="currency_id" class="form-control input-defaulted">
+				  		<option>Choose Bank</option>
+					<option value="214">FIRST CITY MONUMENT BANK PLC</option>
+                    			<option value="215">UNITY BANK PLC</option>
+                    			<option value="221">STANBIC IBTC BANK PLC</option>
+                    			<option value="232">STERLING BANK PLC</option>
+					<option value="304">Stanbic Mobile</option>
+                    			<option value="305">PAYCOM</option>
+                    			<option value="307">Ecobank Mobile</option>
+                    			<option value="309">FBN MOBILE</option>
+					<option value="311">Parkway</option>
+					<option value="315">GTBank Mobile Money</option>
+					<option value="322">ZENITH Mobile</option>
+					<option value="323">ACCESS MOBILE</option>
+					<option value="401">Aso Savings and Loans</option>
+					<option value="044">ACCESS BANK NIGERIA</option>
+					<option value="014">AFRIBANK NIGERIA PLC</option>
+					<option value="063">DIAMOND BANK PLC</option>
+					<option value="050">ECOBANK NIGERIA PLC</option>
+					<option value="084">ENTERPRISE BANK LIMITED</option>
+					<option value="070">FIDELITY BANK PLC</option>
+					<option value="011">FIRST BANK PLC</option>
+					<option value="058">GTBANK PLC</option>
+					<option value="030">HERITAGE BANK</option>
+					<option value="082">KEYSTONE BANK PLC</option>
+					<option value="076">SKYE BANK PLC</option>
+					<option value="068">STANDARD CHARTERED BANK NIGERIA LIMITED</option>
+					<option value="032">UNION BANK OF NIGERIA PLC</option>
+					<option value="033">UNITED BANK FOR AFRICA PLC</option>
+					<option value="035">WEMA BANK PLC</option>
+					<option value="057">ZENITH BANK PLC</option>
+					
+                </select>
+                
                 </div>
+					
+                </div>
+				
+				
                 <input type="button" class="btn btn-block btn-success btn-top-up" name="" value="ADD SMS ACCOUNT">
               </form>
         </div>
@@ -100,22 +326,36 @@
   </div>
   
 </div>
+<!-- /.modal -->
+       
+        <hr />
+        <div class="row">
 
           @foreach ($smswalletdetails as $smswalletdetail)
 
-            <div class="col-md-3 col-xs-6 units">
+            <div class="col-md-3 col-xs-12 units">
                 <!-- small box -->
-                <div class="small-box bg-aqua">
-                <div class="inner">
-                    <h3>SMS Wallet</h3>
-                    <p>Account Name: <span class="username">{{ $smswalletdetail['username'] }}</span></p>
-                    <p>Sms Unit: <span class="unit-balance">{{ number_format($smswalletdetail['balance'], 2) }}</span></p>
-                </div>
-                <div class="icon">
-                    <i class="fa fa-envelope"></i>
-                </div>
-                <a href="#" class="small-box-footer" type="button"  data-toggle="modal" data-target="#top-up-modal">Top up <i class="fa fa-arrow-circle-right"></i></a>
-
+                <div class="small-box">
+			<div class="inner unit">
+			    <p>SMS Account</p>
+			    <hr/>
+			    <p>Account Name: <span class="username">{{ $smswalletdetail['username'] }}</span></p>
+			    <p>Sms Unit: <span class="unit-balance">{{ number_format($smswalletdetail['balance'], 2) }}</span>
+			    	@if($smswalletdetail['balance'] < 50 && $smswalletdetail['balance'] >10)
+					<p class="text-warning">Balance is getting low</p>
+				@elseif($smswalletdetail['balance'] < 10) 
+					<p class="text-danger">Balance is low</p>
+				@else 
+					<p></p>
+				@endif
+			    </p>
+			</div>
+			
+			<div class="icon">
+			    <i class="fa fa-envelope"></i>
+			</div>	
+			<a href="#" class="btn-success btn pull-right" type="button"  data-toggle="modal" data-target="#top-up-modal">Top up <i class="fa fa-arrow-circle-right"></i></a>
+			
                 </div>
             </div>
 
@@ -146,7 +386,7 @@
                   <input class="form-control email" name="email" type="email" readonly>
                 </div>
                 <div class="form-group">
-                  <label>Api Id</label>
+                  <label>SMS Unit</label>
                   <input class="form-control app_id" name="app_id" type="text" readonly>
                 </div>
                  <div class="form-group">
@@ -163,7 +403,9 @@
           </div>
           <!-- /.modal-dialog -->
         </div>
-        <!-- /.modal -->		
+        <!-- /.modal -->	
+        
+        
       
     <!-- /.content -->
   </div>
@@ -400,32 +642,21 @@
     }
     //function to generate reference key 
     function getReferKey(){
-      return "Rf"+ Math.random(1000,9999);
+      return "Rf"+ Math.random()*1000;
     }
     
-    //function to get api key 
-    function getSmsApiKey(username, password){
-      var xmlHttp = new XMLHttpRequest();
-      try{
-      
-      xmlHttp.open( "post",'http://api.ebulksms.com:8080/getapikey.json', true ); // false for synchronous request
-      //xmlHttp.setRequestHeader("Accept", "");
-      xmlHttp.setRequestHeader('Content-type', 'application/json');
-      xmlHttp.send(JSON.stringify({
-            "auth": {
-              "username":"samfield4sure@gmail.com",
-              "password":"samfield"
-              }
-            }));
-            
-          alert(xmlHttp.responseText);
-          }catch(e){
-            alert(e);
-          }
-          
-    }
-  //	getSmsApiKey("samfield4sure@gmail.com","samfield");
-    
+ 
+      //Balance Refresh Button to get new Balance
+var refBut = document.getElementById("balRefre");
+refBut.addEventListener('click', function(){
+        //calling the get balance function to refresh the wallet balance with current balance
+        var token = getToken("ts_Q8PES8G6QJFI2RI1THN1","ts_AM2PIJ8VTPYLBK1K6EJDEXD9STLC6G");
+        var bal = getWalletBalance(token);
+        document.getElementById('balance').innerHTML = "Balance:"+bal;
+});
     
   </script>
 @endsection
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
