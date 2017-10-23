89bf33aae3414294e1d8e43f690936bd8113bc72
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-23 10:45:29 +0100

    changed transfer to wallet image

--- resources/views/transfer-to-wallet.blade.php
+++ resources/views/transfer-to-wallet.blade.php
@@ -8,7 +8,7 @@
 <link rel="stylesheet" href="/css/form.css">
             
  <div class="col-md-6 col-sm-6">
- <img src="https://cdn2.iconfinder.com/data/icons/euro-corporative/512/bank_finance_cash_dollar_purchase_money_transfer_buy_v2_euro_european-512.png" width="250" height="250">
+ <img src="https://ferpay.com/fpmedia/panel/svg/ewallet-transfer-bank.svg" width="250" height="250">
 
         <h4 class="intro text-left" >Transfer to another Wallet account </h4>
        {{-- @if($failed)) 
