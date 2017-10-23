3f547647ea620db17acf91bc332d9371c99ffd8f
AniekanE <hayley.essien94@gmail.com>
2017-10-23 12:33:04 +0100

    Added a link for the back button on success page

--- resources/views/transactionsuccess.blade.php
+++ resources/views/transactionsuccess.blade.php
@@ -7,12 +7,12 @@
 
             <!-- Modal Header -->
             <div class="modal-header">
-            	<span><i class="fa fa-arrow-left" aria-hidden="true"></i></span>
+            	<span  href='https://finance.hotels.ng/admin'><i class="fa fa-arrow-left" aria-hidden="true"></i></span>
                 <h4 class="modal-title">TRANSFER RULES</h4>
             </div>
             <!-- Modal Body -->
             <div class="modal-body">
-                <h4>YOUR TRANSACTION WAS SUCCESSFULL</h4>
+                <h4>YOUR TRANSACTION WAS SUCCESSFUL</h4>
             </div>
 
 </div>
