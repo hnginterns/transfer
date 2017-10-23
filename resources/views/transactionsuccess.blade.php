b42a86ad0c3acd742020c03d460b63d0a1661271
Daniella Mato <dmpire2@gmail.com>
2017-10-23 11:12:42 +0100

    Add back button to transaction modal

--- resources/views/transactionsuccess.blade.php
+++ resources/views/transactionsuccess.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.user')
 
 @section('content')
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
  <div class="col-md-12">
 
             <!-- Modal Header -->
             <div class="modal-header">
+            	<span><i class="fa fa-arrow-left" aria-hidden="true"></i></span>
                 <h4 class="modal-title">TRANSFER RULES</h4>
             </div>
             <!-- Modal Body -->
