3a2e9276d34bf16d4cf8eab383d15226e49c0d1a
Nsenam Udoh <nsenamudoh@gmail.com>
2017-10-26 14:22:40 +0100

    updated phonetopup section

--- resources/views/layouts/user-sidebar.blade.php
+++ resources/views/layouts/user-sidebar.blade.php
@@ -17,7 +17,11 @@
                 </a>
               </li> <br>
               
-              
+              <li>
+                <a data-toggle="modal" data-target="#PurchaseTopUp" >
+                <i class="fa fa-mobile fa-2x"></i> Fund Phone TopUp Wallet
+                </a>
+              </li> <br>
               
              
                                 
@@ -37,3 +41,34 @@
 
           </ul>
 </div>
+
+<div class="container">
+                            <!-- Trigger the modal with a button -->
+                            <!-- Modal -->
+                            <div class="modal fade" id="PurchaseTopUp" role="dialog">
+                                <div class="modal-dialog">
+                                    <!-- Modal content-->
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                            <h4 class="modal-title">Transfer To Service Provider</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form action="{{config('app.url')}}/admin/transfer/topupUser" method="post" accept-charset="utf-8">
+                                                <div class="modal-body" style="padding: 5px;">
+                                                                                                    <div class="row">
+                                                        <div class="col-lg-12 col-md-12 col-sm-12" style="padding-bottom: 10px;">
+                                                            <input class="form-control" name="amount" placeholder="Amount" type="number" required />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="panel-footer" style="margin-bottom:-14px;">
+                                                    <button type="submit" class="btn btn-success">Purchase</button>
+                                                    <button style="float: right;" type="button" class="btn btn-default btn-close" data-dismiss="modal">Close</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
