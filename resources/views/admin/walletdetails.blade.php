be6af5ae6e7390ce8eb6a8e4d8a85fda8118fffa
ceoehis <ceo.ehis@outlook.com>
2017-10-28 00:03:39 +0100

    debug beneficiaries tab on viewwallet page

--- resources/views/admin/walletdetails.blade.php
+++ resources/views/admin/walletdetails.blade.php
@@ -232,8 +232,7 @@
 
               <!-- /.tab-pane -->
               <div class="tab-pane" id="transfers">
-
-            <!-- @if(!empty($bankTransaction)) -->
+                @if(!empty($users))
                <table class="table">
                     <thead>
                         <tr>
@@ -245,12 +244,12 @@
                     </thead>
 
                     <tbody style="color: #595757;">
-                      @forelse($bankTransaction as $transaction)
+                      @forelse($users as $user)
                       <tr>
-                        <td style="color: #595757;"> {{ $transaction->first_name }}</td>
-                        <td style="color: #595757;"> {{ $transaction->last_name }}</td>
-                        <td style="color: #595757;"> {{ $transaction->username }}</td>
-                        <td style="color: #595757;"> {{ $transaction->email }}</td>
+                        <td style="color: #595757;"> {{ $user->first_name }}</td>
+                        <td style="color: #595757;"> {{ $user->last_name }}</td>
+                        <td style="color: #595757;"> {{ $user->username }}</td>
+                        <td style="color: #595757;"> {{ $user->email }}</td>
                       </tr>
                       @empty
                         <p> No User has been attached to this wallet.</p>
@@ -258,10 +257,9 @@
                     </tbody>
 
                 </table>
-            <!-- @else -->
+            @else
               <h2> No user has been attached to this wallet yet </h2>
-            <!-- @endif -->
-
+            @endif
               </div>
 
 
