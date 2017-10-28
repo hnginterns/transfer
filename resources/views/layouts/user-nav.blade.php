0710f45a49ef43a67fe74977df68430e1b23f858
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-28 20:10:47 +0100

    Update user-nav.blade.php

--- resources/views/layouts/user-nav.blade.php
+++ resources/views/layouts/user-nav.blade.php
@@ -7,12 +7,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>    
-            <!-- Header Navbar -->
-  <nav class="navbar navbar-static-top" style="" role="navigation">
-    <!-- Sidebar toggle button-->
-    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-      <span class="sr-only">Toggle navigation</span>
-</a>
+     
           </div>
          
           <div class="collapse navbar-collapse" id="navbar">
