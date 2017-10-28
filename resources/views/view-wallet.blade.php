58357fb62a28e35d0a95772dcab6d67e3f82e548
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-28 11:35:39 +0100

    change beneficiaries transfer to simple descriptio

--- resources/views/view-wallet.blade.php
+++ resources/views/view-wallet.blade.php
@@ -101,7 +101,7 @@ tr:nth-child(even) {
           <ul class="nav nav-pills nav-justified ">
             <li class="active"><a data-toggle="pill" href="#home">Wallet Transfer History</a></li>
             <li><a data-toggle="pill" href="#menu1">Beneficiaries List</a></li>
-            <li><a data-toggle="pill" href="#menu2">Beneficiaries Transfer</a></li>
+            <li><a data-toggle="pill" href="#menu2">Beneficiaries Transaction History</a></li>
           </ul>
         <div class="tab-content">
         <div id="home" class="tab-pane fade in active">
