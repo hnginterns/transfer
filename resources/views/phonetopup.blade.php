93b9eabde9fa671d21dee23026a035d4e28aafed
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-29 15:02:01 +0100

    Update phonetopup.blade.php

--- resources/views/phonetopup.blade.php
+++ resources/views/phonetopup.blade.php
@@ -91,9 +91,9 @@ i.can {
   <div class="col-md-12 text-center">
 <hr>
         <ul class="nav nav-pills nav-justified ">
-            <li class="active"><a data-toggle="pill" href="#contactlistbox"><b><h2>Top Up</h2><b></a></li>
-            <li><a data-toggle="pill" href="#fundhistorybox"><b><h2>Funding History</h2><b></a></li>
-            <li><a data-toggle="pill" href="#topuphistorybox"><b><h2>Topup History</h2><b></a></li>
+            <li class="active"><a data-toggle="pill" href="#contactlistbox"><b>Top Up<b></a></li>
+            <li><a data-toggle="pill" href="#fundhistorybox"><b>Funding History<b></a></li>
+            <li><a data-toggle="pill" href="#topuphistorybox"><b>Topup Historyb></a></li>
         </ul>
 
     </div>
