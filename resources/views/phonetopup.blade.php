390cb8af27b3a307896138bb2163d2d69875dddd
godfredakpan <31159357+godfredakpan@users.noreply.github.com>
2017-10-29 17:34:28 +0100

    fixing top up all button

--- resources/views/phonetopup.blade.php
+++ resources/views/phonetopup.blade.php
@@ -153,10 +153,12 @@ i.can {
 
 </div>
 <br>
-<button type="submit" class="btn btn-success pull-right" style="margin-left: 5px; margin-bottom: 3px;">Top up all</button>
+
+<button type="submit" class="btn btn-success">Top up all</button>
 <a href="#dataModal" class="btn btn-info pull-right" style="margin-right: 5px; margin-bottom: 3px;" data-toggle="modal">Top-up Data</a>
 <br><br>
 <hr>
+
 
     <div class="table table-responsive">
       <table class="table" id="contact-table">
