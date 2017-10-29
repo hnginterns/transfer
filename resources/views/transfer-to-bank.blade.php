ba3868994e258e681e8556b571a78f6db892286e
Ikechukwu Nwakanma <patjickconsult@gmail.com>
2017-10-29 19:56:35 +0100

    modified the transfer to beneficiary validation

--- resources/views/transfer-to-bank.blade.php
+++ resources/views/transfer-to-bank.blade.php
@@ -7,33 +7,25 @@
 
 <link rel="stylesheet" href="/css/form.css">
             <div class="col-md-6 col-sm-6">
+              @if (session('data'))
             <img src="https://ferpay.com/fpmedia/panel/svg/ewallet-transfer-bank.svg" width="300" height="300">
-                  @if(session('failed'))
-                  
-                  <div class="alert alert-danger">
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                    <strong>{{ session('failed') }}</strong> 
-                  </div>
-                  @elseif(session('status'))
-                  
-                  <div class="alert alert-success">
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                    <strong>{{ session('status') }}</strong> Alert body ...
-                  </div>
-                  
-                  
-                  @endif
+                 
                   <h4 class="intro text-center">Transfer To Beneficiary </h4>
                   <form class="input-form" action="" method="POST">
                   {{csrf_field()}}
 
+                     <form class="input-form" action="/transfer/beneficiary/{{$wallet->id}}" method="POST">
+                  {{csrf_field()}}
+                  @foreach(Session::get('data') as $data)
                     <div class="form-group">
-                        <select class="form-control cus-input" name="beneficiary_id">
-                          <option>Select Beneficiary</option>
-                            @foreach($wallet->beneficiary as $key => $beneficiaries)
-                              <option value="{{$beneficiaries->id}}">{{$beneficiaries->name}}</option>
-                            @endforeach
-                        </select>
+                        <input type="hidden" class="form-control cus-input" name="beneficiary_id" value="{{$data['beneficiary_id']}}">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control cus-input" name="beneficiary_name" value="{{$data['beneficiary_name']}}" readonly>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" class="form-control cus-input" name="account_number" value="{{$data['account_number']}}" readonly>
                     </div>
                           
                           <div class="form-group">
@@ -41,22 +33,25 @@
                           </div>
 
                             <div class="form-group">
-                              <select class="form-control cus-input" name="wallet_id" id="wallet_id">
-                                  <option value="{{ $wallet->id }}">{{ $wallet->wallet_name }}</option>
-                                
-                              </select>
+                              <input type="hidden" class="form-control cus-input" name="wallet_id" value="{{$wallet->id}}">
+                            </div>
+
+                            <div class="form-group">
+                              <input type="hidden" class="form-control cus-input" name="wallet_name" value="{{$wallet->wallet_name}}">
                             </div>
 
                             <div class="form-group">
                               <input type="text" class="form-control cus-input" name="narration" id="narration" placeholder="Narration">
                             </div>
-                            
-                        <div class="form-group center-block">    
+                            @endforeach
+                            <div class="form-group center-block">    
                           <button id="" type="submit" class="btn btn-primary pull-right">Transfer</button>
                         </div>
 
-
                   </form>
             </div>
+            @else
+            <p>Please Validate Transfer</p>
+            @endif
 
   @endsection
