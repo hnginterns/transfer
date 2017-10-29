09ed131ebc046cf3cb1f6fa67edffda5ae91a2e4
Daniella Mato <dmpire2@gmail.com>
2017-10-29 15:58:55 +0100

    Fix feature

--- resources/views/phonetopup.blade.php
+++ resources/views/phonetopup.blade.php
@@ -153,8 +153,8 @@ i.can {
 
 </div>
 <br>
-<button type="submit" class="btn btn-success pull-right" style="margin-right: 5px; margin-bottom: 3px;">Top up all</button>
-<button type="submit" class="btn btn-info pull-right" style="margin-left: 5px; margin-bottom: 3px;">Top-up Data</button>
+<button type="submit" class="btn btn-success pull-right" style="margin-left: 5px; margin-bottom: 3px;">Top up all</button>
+<button type="submit" class="btn btn-info pull-right" style="margin-right: 5px; margin-bottom: 3px;">Top-up Data</button>
 <br><br>
 <hr>
 
