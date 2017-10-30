75100b9ca9fba76ff29ff9f11d554b614fdfe82c
Ikechukwu Nwakanma <patjickconsult@gmail.com>
2017-10-30 18:52:48 +0100

    added csrf_field to phonetopup transfer to provider form

--- resources/views/admin/phonetopup/index.blade.php
+++ resources/views/admin/phonetopup/index.blade.php
@@ -229,6 +229,7 @@
                                         </div>
                                         <div class="modal-body">
                                             <form action="{{config('app.url')}}/admin/transfer/topup" method="post" accept-charset="utf-8">
+                                              {{csrf_field()}}
                                                 <div class="modal-body" style="padding: 5px;">
                                                     <div class="row">
                                                         <div class="col-lg-12 col-md-12 col-sm-12" style="padding-bottom: 10px;">
