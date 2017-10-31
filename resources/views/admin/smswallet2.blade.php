fee0cde6ab9efe676bb986dc8d8a9454fc7eab47
Ikechukwu Nwakanma <patjickconsult@gmail.com>
2017-10-31 20:26:51 +0100

    test

--- resources/views/admin/smswallet2.blade.php
+++ resources/views/admin/smswallet2.blade.php
@@ -363,6 +363,7 @@
                                             <div class="form-group">
                                                 <input type="password" class="form-control" name="otp" placeholder="Enter OTP">
                                             </div>
+                                            @endforeach
                                             <button type="submit" class="btn btn-default btn-block">Submit</button>
                                           </form>
                                         </div>
