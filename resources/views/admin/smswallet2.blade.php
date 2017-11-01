c5334042a8925a8894b05661926d68c6eb200f92
Ikechukwu Nwakanma <patjickconsult@gmail.com>
2017-11-01 10:52:10 +0100

    smswallet transfer to provider fixed

--- resources/views/admin/smswallet2.blade.php
+++ resources/views/admin/smswallet2.blade.php
@@ -296,7 +296,7 @@
               <div class="modal-body">
             <!-- /.box-header -->
             <div class="box-body">
-              <form action="transfer" method="post">
+              <form action="{{config('app.url')}}/admin/transfer" method="post">
                 {{csrf_field()}}
                 <!-- text input -->
                 <div class="form-group">
