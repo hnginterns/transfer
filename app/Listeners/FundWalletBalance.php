a0aa02ecb9df5f572aa16ae4904532d8441669c7
Ikechukwu Nwakanma <patjickconsult@gmail.com>
2017-11-15 10:27:22 +0100

    resolved token error

--- app/Listeners/FundWalletBalance.php
+++ app/Listeners/FundWalletBalance.php
@@ -27,7 +27,7 @@ class FundWalletBalance
         $headers = array('content-type' => 'application/json');
         $query = array('apiKey' => $api_key, 'secret' => $secret_key);
         $body = \Unirest\Request\Body::json($query);
-        $response = \Unirest\Request::post('https://live.moneywaveapi.co/v1/merchant/verify', $headers, $body);
+        $response = \Unirest\Request::post(env('API_KEY_LIVE_URL').'/v1/merchant/verify', $headers, $body);
         $response = json_decode($response->raw_body, true);
         $status = $response['status'];
         if (!$status == 'success') {
